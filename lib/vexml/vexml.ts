85d402f282841178c1158ae7342f81adeeaf4b79
Jared Johnson <jared@jaredjohnson.dev>
2022-04-03 16:36:42 -0400

    Create webpack dev server

--- lib/vexml/vexml.ts
+++ lib/vexml/vexml.ts
@@ -7,11 +7,8 @@ export const load = (file: Blob, elementId: string) => {
     if (xmlString) {
       const parser: DOMParser = new DOMParser();
       const xml = parser.parseFromString(xmlString, 'application/xml');
-      EasyScoreMessageRenderer.render(
-        elementId,
-        xml
-      );
+      EasyScoreMessageRenderer.render(elementId, xml);
     }
   };
   reader.readAsBinaryString(file);
-}
+};
