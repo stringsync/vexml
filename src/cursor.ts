9e10f07d6ee864c9f55fa354e946c4ed47743a57
Jared Johnson <jared@jaredjohnson.dev>
2022-04-04 08:19:41 -0400

    Add Cursor.fromRoot

--- src/cursor.ts
+++ src/cursor.ts
@@ -4,6 +4,10 @@ export class Cursor {
   static fromMusicXml(musicXml: string): Cursor {
     const parser = new DOMParser();
     const root = parser.parseFromString(musicXml, 'application/xml');
+    return Cursor.fromRoot(root);
+  }
+
+  static fromRoot(root: Document): Cursor {
     const path = root.evaluate('/score-partwise/part/measure', root, null, XPathResult.ANY_TYPE, null);
 
     const nodes = new Array<Node>();
