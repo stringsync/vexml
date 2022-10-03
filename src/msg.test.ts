23e35a8c5a253e9990ac5b441b1ae0adc51dc23c
Jared Johnson <jared@jaredjohnson.dev>
2022-10-03 08:11:29 -0400

    Simplify msg tests

--- src/msg.test.ts
+++ src/msg.test.ts
@@ -1,63 +1,11 @@
 import * as msg from './msg';
 
 describe('msg', () => {
-  describe('attributes', () => {
-    it('runs without crashing', () => {
-      expect(msg.attributes).not.toThrow();
+  for (const [name, fn] of Object.entries(msg)) {
+    describe(name, () => {
+      it('runs without crashing', () => {
+        expect(fn).not.toThrow();
+      });
     });
-  });
-
-  describe('barline', () => {
-    it('runs without crashing', () => {
-      expect(msg.barline).not.toThrow();
-    });
-  });
-
-  describe('beam', () => {
-    it('runs without crashing', () => {
-      expect(msg.beam).not.toThrow();
-    });
-  });
-
-  describe('measureEnd', () => {
-    it('runs without crashing', () => {
-      expect(msg.measureEnd).not.toThrow();
-    });
-  });
-
-  describe('measureStart', () => {
-    it('runs without crashing', () => {
-      expect(msg.measureStart).not.toThrow();
-    });
-  });
-
-  describe('notation', () => {
-    it('runs without crashing', () => {
-      expect(msg.notation).not.toThrow();
-    });
-  });
-
-  describe('note', () => {
-    it('runs without crashing', () => {
-      expect(msg.note).not.toThrow();
-    });
-  });
-
-  describe('partEnd', () => {
-    it('runs without crashing', () => {
-      expect(msg.partEnd).not.toThrow();
-    });
-  });
-
-  describe('partStart', () => {
-    it('runs without crashing', () => {
-      expect(msg.partStart).not.toThrow();
-    });
-  });
-
-  describe('voiceEnd', () => {
-    it('runs without crashing', () => {
-      expect(msg.voiceEnd).not.toThrow();
-    });
-  });
+  }
 });
