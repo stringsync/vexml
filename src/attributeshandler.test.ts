e52442810c36bb53af34980ff0ceeac3ec2e7711
Jared Johnson <jared@jaredjohnson.dev>
2022-11-24 16:06:47 -0500

    Test attributes clef handling

--- src/attributeshandler.test.ts
+++ src/attributeshandler.test.ts
@@ -1,7 +1,40 @@
 import { AttributesHandler } from './attributeshandler';
+import { DEFAULT_CONFIG } from './di';
+import * as msg from './msg';
+import { NoopReceiver } from './noopreceiver';
+import { AttributesMessage } from './types';
+import * as xml from './xml';
 
 describe(AttributesHandler, () => {
-  it.todo('sends an attributes message with clefs');
+  let receiver: NoopReceiver;
+  let attributesHandler: AttributesHandler;
+
+  let receiverSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    receiver = new NoopReceiver();
+    attributesHandler = new AttributesHandler({ config: DEFAULT_CONFIG });
+
+    receiverSpy = jest.spyOn(receiver, 'onMessage');
+  });
+
+  it('sends an attributes message with clefs', () => {
+    const attributes = xml.attributes({
+      clefs: [xml.clef({ staff: 1 }), xml.clef({ staff: 2 })],
+    });
+
+    attributesHandler.sendMessages(receiver, { node: attributes });
+
+    expect(receiverSpy).toHaveBeenCalledOnce();
+    expect(receiverSpy).toHaveBeenLastCalledWith<[AttributesMessage]>(
+      msg.attributes({
+        clefs: [
+          { staff: 1, sign: 'G', line: 2, octaveChange: undefined },
+          { staff: 2, sign: 'G', line: 2, octaveChange: undefined },
+        ],
+      })
+    );
+  });
 
   it.todo('sends an attributes message with times');
 
