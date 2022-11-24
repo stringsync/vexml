85fdee249831b50fb7d5796e59519abada738753
Jared Johnson <jared@jaredjohnson.dev>
2022-11-24 12:56:49 -0500

    Add AttributesHandler skeleton and di

---
+++ src/attributeshandler.test.ts
@@ -0,0 +1,9 @@
+import { AttributesHandler } from './attributeshandler';
+
+describe(AttributesHandler, () => {
+  it.todo('sends an attributes message with clefs');
+
+  it.todo('sends an attributes message with times');
+
+  it.todo('sends an attributes message with keys');
+});
