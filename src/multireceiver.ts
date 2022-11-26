ebc3f0bb90344eb72407dd4588232c870271df11
Jared Johnson <jared@jaredjohnson.dev>
2022-11-26 12:02:41 -0500

    Create multireceiver

---
+++ src/multireceiver.ts
@@ -0,0 +1,15 @@
+import { VexmlMessage, VexmlMessageReceiver } from './types';
+
+export class MultiReceiver implements VexmlMessageReceiver {
+  static of(...receivers: VexmlMessageReceiver[]): MultiReceiver {
+    return new MultiReceiver(receivers);
+  }
+
+  private constructor(private receivers: VexmlMessageReceiver[]) {}
+
+  onMessage(message: VexmlMessage): void {
+    for (const receiver of this.receivers) {
+      receiver.onMessage(message);
+    }
+  }
+}
