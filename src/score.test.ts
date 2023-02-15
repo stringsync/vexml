4bc882283dc66fb003c55950f730696bb8c7fce1
Jared Johnson <jared@jaredjohnson.dev>
2023-02-15 06:01:33 -0500

    Scaffold Score tests

---
+++ src/score.test.ts
@@ -0,0 +1,19 @@
+import { Score } from './score';
+
+describe(Score, () => {
+  describe('getPartCount', () => {
+    it.todo('returns the number of <score-part> elements in the document');
+  });
+
+  describe('getPartIds', () => {
+    it.todo('returns the part IDs in the document');
+  });
+
+  describe('getPartNames', () => {
+    it.todo('returns the part names in the document');
+  });
+
+  describe('getMeasures', () => {
+    it.todo('returns the measures in the document');
+  });
+});
