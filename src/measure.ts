b37d94b2ce0a8c1f2c9258223e0677c5ce95295e
Jared Johnson <jared@jaredjohnson.dev>
2023-02-15 05:54:39 -0500

    Add measure and score

---
+++ src/measure.ts
@@ -0,0 +1,43 @@
+import { NamedNode } from './util/namednode';
+
+/** Measure is a basic musical data container that has notes and rests. */
+export class Measure {
+  constructor(private node: NamedNode<'measure'>) {}
+
+  /** Returns the measure number or -1 if there was a problem parsing it. */
+  getNumber(): number {
+    const number = this.node.asElement().getAttribute('number');
+    return this.parseIntOr(number, -1);
+  }
+
+  /** Returns whether or not the measure has a specified width */
+  hasWidth(): boolean {
+    return this.node.asElement().hasAttribute('width');
+  }
+
+  /** Returns the specified measured width in tenths or -1 if there was a problem parsing it. */
+  getWidth(): number {
+    const width = this.node.asElement().getAttribute('width');
+    return this.parseIntOr(width, -1);
+  }
+
+  /** Returns the stave count in the measure or 1 if there was a problem parsing it. */
+  getStaveCount(): number {
+    const staves = this.node.asElement().getElementsByTagName('staves').item(0)?.textContent;
+    return this.parseIntOr(staves, 1);
+  }
+
+  /** Parses a value into an integer and returns the fallback if there are any problems parsing it. */
+  private parseIntOr(value: any, fallback: number): number {
+    if (typeof value !== 'string') {
+      return fallback;
+    }
+
+    const int = parseInt(value, 10);
+    if (isNaN(int)) {
+      return fallback;
+    }
+
+    return int;
+  }
+}
--- src/score.ts
+++ src/score.ts
@@ -1,33 +1,34 @@
+import { Measure } from './measure';
 import { NamedNode } from './util/namednode';
 
+/** Score is the entrypoint of a MusicXML document. */
 export class Score {
   constructor(private node: NamedNode<'score-partwise'>) {}
 
+  /** Returns the part count in the score. */
   getPartCount(): number {
     return this.node.asElement().getElementsByTagName('score-part').length;
   }
 
+  /** Returns an array of part IDs in the score in the order they appear. Each part ID should be unique. */
   getPartIds(): string[] {
-    const result = new Array<string>();
-    const scoreParts = this.node.asElement().getElementsByTagName('score-part');
-    for (const scorePart of scoreParts) {
-      const id = scorePart.getAttribute('id');
-      if (id) {
-        result.push(id);
-      }
-    }
-    return result;
+    return Array.from(this.node.asElement().getElementsByTagName('score-part'))
+      .filter((scorePart) => scorePart.hasAttribute('id'))
+      .map((scorePart) => scorePart.getAttribute('id'))
+      .filter((id): id is string => typeof id === 'string');
   }
 
+  /** Returns an array of part names in the score in the order they appear. Part names can be duplicated. */
   getPartNames(): string[] {
-    const result = new Array<string>();
-    const partNames = this.node.asElement().getElementsByTagName('part-name');
-    for (const partName of partNames) {
-      const content = partName.textContent;
-      if (content) {
-        result.push(content);
-      }
-    }
-    return result;
+    return Array.from(this.node.asElement().getElementsByTagName('part-name'))
+      .map((partName) => partName.textContent)
+      .filter((textContent): textContent is string => typeof textContent === 'string');
+  }
+
+  /** Returns an array of measures in the order they appear. */
+  getMeasures(): Measure[] {
+    return Array.from(this.node.asElement().getElementsByTagName('measure'))
+      .map((measure) => NamedNode.of<'measure'>(measure))
+      .map((node) => new Measure(node));
   }
 }
