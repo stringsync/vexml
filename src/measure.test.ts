2008f52ceb6166b19b7900fd528280f923f5a773
Jared Johnson <jared@jaredjohnson.dev>
2023-02-15 06:38:36 -0500

    Scaffold measure tests

---
+++ src/measure.test.ts
@@ -0,0 +1,33 @@
+import { Measure } from './measure';
+
+describe(Measure, () => {
+  describe('getNumber', () => {
+    it.todo('returns the measure number');
+
+    it.todo('returns -1 when the measure number is missing');
+
+    it.todo('returns -1 when the measure number is invalid');
+  });
+
+  describe('hasWidth', () => {
+    it.todo('returns true when the measure width is set');
+
+    it.todo('returns false when the measure width is not set');
+  });
+
+  describe('getWidth', () => {
+    it.todo('returns the measure width');
+
+    it.todo('returns -1 when the measure width is missing');
+
+    it.todo('returns -1 when the measure width is invalid');
+  });
+
+  describe('getStaveCount', () => {
+    it.todo('returns the stave count');
+
+    it.todo('returns 1 when the measure stave count is missing');
+
+    it.todo('returns 1 when the measure stave count is invalid');
+  });
+});
