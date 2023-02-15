838ed3a828161b2cdf89194203612160254ccebb
Jared Johnson <jared@jaredjohnson.dev>
2023-02-15 06:32:11 -0500

    Test xml module

---
+++ src/xml.test.ts
@@ -0,0 +1,23 @@
+import * as xml from './xml';
+
+type FnNames = keyof typeof xml;
+
+const AUTO_CRASH_TEST_SKIP_NAMES: Set<FnNames> = new Set(['createElement']);
+
+describe('xml', () => {
+  describe('createElement', () => {
+    it('runs without crashing', () => {
+      expect(() => xml.createElement('foo')).not.toThrow();
+    });
+  });
+
+  for (const [name, fn] of Object.entries(xml)) {
+    if (!AUTO_CRASH_TEST_SKIP_NAMES.has(name as FnNames)) {
+      describe(name, () => {
+        it('runs without crashing', () => {
+          expect(fn).not.toThrow();
+        });
+      });
+    }
+  }
+});
