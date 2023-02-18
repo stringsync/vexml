6c402c8226ea22b17266e0573633b876be5dadce
Jared Johnson <jared@jaredjohnson.dev>
2023-02-18 15:57:50 -0500

    Translate fifths to key signature in Key class

--- src/key.ts
+++ src/key.ts
@@ -14,4 +14,40 @@ export class Key {
     const fifths = this.node.asElement().getElementsByTagName('fifths').item(0)?.textContent;
     return parse.intOrDefault(fifths, 0);
   }
+
+  /** Returns the key signature based on the fifths count. */
+  getKeySignature(): string {
+    switch (this.getFifthsCount()) {
+      case 1:
+        return 'G';
+      case 2:
+        return 'D';
+      case 3:
+        return 'A';
+      case 4:
+        return 'E';
+      case 5:
+        return 'B';
+      case 6:
+        return 'F#';
+      case 7:
+        return 'C#';
+      case -1:
+        return 'F';
+      case -2:
+        return 'Bb';
+      case -3:
+        return 'Eb';
+      case -4:
+        return 'Ab';
+      case -5:
+        return 'Cb';
+      case -6:
+        return 'Gb';
+      case -7:
+        return 'Cb';
+      default:
+        return 'C';
+    }
+  }
 }
