6427a34e986bd57235df92959e52f4833b0130d4
Jared Johnson <jared@jaredjohnson.dev>
2023-02-19 13:10:31 -0500

    Add barline getters

--- src/barline.test.ts
+++ src/barline.test.ts
@@ -87,4 +87,60 @@ describe(Barline, () => {
       expect(barline.getLocation()).toBe('right');
     });
   });
+
+  describe('isEnding', () => {
+    it('returns true when the barline has an ending', () => {
+      const node = xml.barline({ ending: xml.ending() });
+      const barline = new Barline(node);
+      expect(barline.isEnding()).toBeTrue();
+    });
+
+    it('returns false when the barline does not have an ending', () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.isEnding()).toBeFalse();
+    });
+  });
+
+  describe('getEndingText', () => {
+    it('returns the ending text of the barline', () => {
+      const node = xml.barline({ ending: xml.ending({ textContent: 'foo' }) });
+      const barline = new Barline(node);
+      expect(barline.getEndingText()).toBe('foo');
+    });
+
+    it('defaults to an empty string when the barline does not have an ending', () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.getEndingText()).toBe('');
+    });
+  });
+
+  describe('getEndingNumber', () => {
+    it('returns the ending number of the barline', () => {
+      const node = xml.barline({ ending: xml.ending({ number: '2' }) });
+      const barline = new Barline(node);
+      expect(barline.getEndingNumber()).toBe('2');
+    });
+
+    it(`defaults to '1' when the ending number is missing`, () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.getEndingNumber()).toBe('1');
+    });
+  });
+
+  describe('getEndingType', () => {
+    it.each(['start', 'stop', 'discontinue'])(`returns the ending type of the barline: '%s'`, (endingType) => {
+      const node = xml.barline({ ending: xml.ending({ type: endingType }) });
+      const barline = new Barline(node);
+      expect(barline.getEndingType()).toBe(endingType);
+    });
+
+    it(`defaults to 'start' when the barline does not have an ending type`, () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.getEndingType()).toBe('start');
+    });
+  });
 });
