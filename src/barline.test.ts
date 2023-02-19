f23e9647c05f83941fa6e966069b1a56fad1c6b2
Jared Johnson <jared@jaredjohnson.dev>
2023-02-19 12:39:10 -0500

    Add barline getters

--- src/barline.test.ts
+++ src/barline.test.ts
@@ -33,4 +33,58 @@ describe(Barline, () => {
       expect(barline.getBarStyle()).toBe('regular');
     });
   });
+
+  describe('isRepeat', () => {
+    it('returns true when a repeat is present', () => {
+      const node = xml.barline({ repeat: xml.repeat() });
+      const barline = new Barline(node);
+      expect(barline.isRepeat()).toBeTrue();
+    });
+
+    it('returns false when a repeat is absent', () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.isRepeat()).toBeFalse();
+    });
+  });
+
+  describe('getRepeatDirection', () => {
+    it.each(['backward', 'forward'])(`returns the repeat direction of the barline: '%s'`, (direction) => {
+      const node = xml.barline({ repeat: xml.repeat({ direction }) });
+      const barline = new Barline(node);
+      expect(barline.getRepeatDirection()).toBe(direction);
+    });
+
+    it('defaults to null when the repeat direction is invalid', () => {
+      const node = xml.barline({ repeat: xml.repeat({ direction: 'foo' }) });
+      const barline = new Barline(node);
+      expect(barline.getRepeatDirection()).toBeNull();
+    });
+
+    it('defaults to null when the repeat direction is absent', () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.getRepeatDirection()).toBeNull();
+    });
+  });
+
+  describe('getLocation', () => {
+    it.each(['right', 'left', 'middle'])(`returns the location of the barline: '%s'`, (location) => {
+      const node = xml.barline({ location });
+      const barline = new Barline(node);
+      expect(barline.getLocation()).toBe(location);
+    });
+
+    it(`defaults to 'right' when the location is invalid`, () => {
+      const node = xml.barline({ location: 'foo' });
+      const barline = new Barline(node);
+      expect(barline.getLocation()).toBe('right');
+    });
+
+    it(`defaults to 'right' when the location is missing`, () => {
+      const node = xml.barline();
+      const barline = new Barline(node);
+      expect(barline.getLocation()).toBe('right');
+    });
+  });
 });
