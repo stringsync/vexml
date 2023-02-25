4b6c46da721622a71b34f003a6326e9d00726976
Jared Johnson <jared@jaredjohnson.dev>
2023-02-25 14:52:46 -0500

    Test enums module

--- src/enums.ts
+++ src/enums.ts
@@ -3,10 +3,11 @@ type Values<T extends readonly any[]> = T extends readonly (infer U)[] ? U : nev
 type EnumValues<T extends Enum<any>> = T extends Enum<infer U> ? Values<U> : never;
 
 /** An enumeration of string values. */
-class Enum<T extends readonly string[]> {
+export class Enum<T extends readonly string[]> {
   constructor(private choices: T) {}
 
   /** Type predicate that returns whether or not the value is one of the choices. */
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   includes(value: any): value is Values<T> {
     return this.choices.includes(value);
   }
