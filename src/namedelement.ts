90e2a720113e039bfc0b139c1a006e9ff4170068
Jared Johnson <jared@jaredjohnson.dev>
2023-02-25 07:17:30 -0500

    Test attr and add append to named element

--- src/namedelement.ts
+++ src/namedelement.ts
@@ -1,7 +1,8 @@
 import { Attr } from './attr';
 
 /**
- * A wrapper class that is used to facilitate compile-time checking on an element's name.
+ * A wrapper class that is used to facilitate compile-time checking on an element's name and provide conveinence methods
+ * on top of the Element api.
  *
  * For example, requiring a signature to use NamedElement<'foo'> will force the caller to assert that an element's name
  * is foo. As long as types aren't being dynamically casted, this is also backed by the compiler.
@@ -24,15 +25,20 @@ export class NamedElement<T extends string> {
     return this.element;
   }
 
-  /** Returns all _descendant_ node matching the tag name. */
-  all<S extends string>(tagName: S): Array<NamedElement<S>> {
-    return Array.from(this.element.getElementsByTagName(tagName)).map((node) => NamedElement.of<S>(node));
+  /** Appends the elements as direct children. */
+  append(...elements: NamedElement<string>[]): void {
+    this.element.append(...elements.map((element) => element.native()));
   }
 
   /** Returns the first _descendant_ node matching the tag name. Defaults to null. */
   first<S extends string>(tagName: S): NamedElement<S> | null {
     const element = this.element.getElementsByTagName(tagName).item(0);
     return element ? NamedElement.of(element) : null;
+  }
+
+  /** Returns all _descendant_ node matching the tag name. */
+  all<S extends string>(tagName: S): Array<NamedElement<S>> {
+    return Array.from(this.element.getElementsByTagName(tagName)).map((node) => NamedElement.of<S>(node));
   }
 
   /** Returns the next _sibling_ node matching the tag name. */
@@ -64,7 +70,7 @@ export class NamedElement<T extends string> {
   /** Returns an attr wrapper for the attribute of the given name. */
   attr(name: string): Attr<null> {
     const value = this.element.getAttribute(name);
-    return new Attr(value, null);
+    return Attr.of(value);
   }
 
   /** Returns the text content of the node. Defaults to empty string. */
