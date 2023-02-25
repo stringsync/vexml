e9dc1383d2e530e1b413f0a2b8c414b12ceba4bf
Jared Johnson <jared@jaredjohnson.dev>
2023-02-25 14:52:46 -0500

    Test enums module

--- src/enums.test.ts
+++ src/enums.test.ts
@@ -1,5 +1,15 @@
-describe('enums', () => {
-  it('loads', () => {
-    expect(() => import('./enums')).not.toThrow();
+import { Enum } from './enums';
+
+describe(Enum, () => {
+  describe('includes', () => {
+    it('returns true when the value is part of the choices', () => {
+      const foo = new Enum(['foo'] as const);
+      expect(foo.includes('foo')).toBeTrue();
+    });
+
+    it('returns false when the value is not part of the choices', () => {
+      const foo = new Enum(['foo'] as const);
+      expect(foo.includes('bar')).toBeFalse();
+    });
   });
 });
