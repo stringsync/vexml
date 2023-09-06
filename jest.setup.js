10076a889ffd0f57af10f6a25139db2ffd0add7b
Jared Johnson <jared@jaredjohnson.dev>
2023-09-06 17:15:41 -0400

    Add threshold to image diff

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { toMatchImageSnapshot } = require('jest-image-snapshot');
+const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  failureThreshold: 1,
+  failureThresholdType: 'percent',
+});
 
 expect.extend({ toMatchImageSnapshot });
