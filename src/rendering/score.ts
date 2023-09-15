7098ccb5ad8b55cb58e14ee9e595625b7d384726
Jared Johnson <jared@jaredjohnson.dev>
2023-09-15 08:57:46 -0400

    Tidy rendering.Score

--- src/rendering/score.ts
+++ src/rendering/score.ts
@@ -1,6 +1,7 @@
 import { System, SystemRendering } from './system';
 import * as musicxml from '@/musicxml';
 import * as vexflow from 'vexflow';
+import * as util from '@/util';
 
 const END_BARLINE_OFFSET = 1;
 const DEFAULT_SYSTEM_DISTANCE = 80;
@@ -11,10 +12,9 @@ export type ScoreRendering = {
 };
 
 /**
- * Represents a Score in a musical composition, serving as the top-level container
- * for all musical elements and metadata. The Score encompasses the entirety of a piece,
- * housing individual parts, systems, and other musical components. It also provides
- * contextual information like title, composer, and other pertinent details.
+ * Represents a Score in a musical composition, serving as the top-level container for all musical elements and
+ * metadata. The Score encompasses the entirety of a piece, housing individual parts, systems, and other musical
+ * components. It also provides contextual information like title, composer, and other pertinent details.
  */
 export class Score {
   private system: System;
@@ -31,6 +31,7 @@ export class Score {
     this.systemLayout = opts.systemLayout;
   }
 
+  /** Creates a Score rendering object. */
   static create(musicXml: musicxml.MusicXml): Score {
     const scorePartwise = musicXml.getScorePartwise();
     const parts = scorePartwise?.getParts() ?? [];
@@ -43,6 +44,7 @@ export class Score {
     return new Score({ system, staffLayouts, systemLayout });
   }
 
+  /** Renders a Score. */
   render(opts: { element: HTMLDivElement | HTMLCanvasElement; width: number }): ScoreRendering {
     // Track the system rendering results.
     const systemRenderings = new Array<SystemRendering>();
@@ -66,7 +68,7 @@ export class Score {
 
       // Height is calculated during render time to avoid duplicate work that would've been done if we were using
       // instance methods on the rendering.Stave object.
-      const maxY = Math.max(
+      const maxY = util.math.max([
         y,
         ...systemRendering.parts
           .flatMap((part) => part.measures)
@@ -74,8 +76,8 @@ export class Score {
           .map((stave) => {
             const box = stave.vexflow.stave.getBoundingBox();
             return box.getY() + box.getH();
-          })
-      );
+          }),
+      ]);
       const height = maxY - y;
 
       y += height;
