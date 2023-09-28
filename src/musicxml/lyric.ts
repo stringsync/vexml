a0f215786731effe3af0fff77d53e34b09f799d4
Jared Johnson <jared@jaredjohnson.dev>
2023-09-28 09:28:42 -0400

    Add lyric component getter

--- src/musicxml/lyric.ts
+++ src/musicxml/lyric.ts
@@ -1,11 +1,10 @@
 import { NamedElement } from '../util';
-import { SyllabicType } from './enums';
+import { SYLLABIC_TYPES, SyllabicType } from './enums';
 
 export type LyricComponent =
   | { type: 'syllabic'; value: SyllabicType }
   | { type: 'text'; value: string }
-  | { type: 'elision'; value: string }
-  | { type: 'extend' };
+  | { type: 'elision'; value: string };
 
 /**
  * The <lyric> element represents text underlays for lyrics.
@@ -16,7 +15,41 @@ export class Lyric {
   constructor(private element: NamedElement<'lyric'>) {}
 
   /** Returns the verse number the lyric belongs to. Defaults to 1. */
-  getVerseNumber(): string {
-    return this.element.attr('number').withDefault('1').str();
+  getVerseNumber(): number {
+    return this.element.attr('number').withDefault(1).int();
+  }
+
+  /** Returns the components of the lyric. */
+  getComponents(): LyricComponent[] {
+    const components = new Array<LyricComponent>();
+
+    for (const child of this.element.native().children) {
+      const element = NamedElement.of(child);
+
+      if (element.isNamed('syllabic')) {
+        components.push(this.createSyllabic(element));
+      } else if (element.isNamed('text')) {
+        components.push(this.createText(element));
+      } else if (element.isNamed('elision')) {
+        components.push(this.createElision(element));
+      }
+    }
+
+    return components;
+  }
+
+  private createSyllabic(syllabic: NamedElement<'syllabic'>): Extract<LyricComponent, { type: 'syllabic' }> {
+    const value = syllabic.content().withDefault('single').enum(SYLLABIC_TYPES);
+    return { type: 'syllabic', value };
+  }
+
+  private createText(text: NamedElement<'text'>): Extract<LyricComponent, { type: 'text' }> {
+    const value = text.content().withDefault('').str();
+    return { type: 'text', value };
+  }
+
+  private createElision(elision: NamedElement<'elision'>): Extract<LyricComponent, { type: 'elision' }> {
+    const value = elision.content().withDefault('').str();
+    return { type: 'elision', value };
   }
 }
