a5508606af8d82a6e954425a1db31aa07af9c7fc
Jared Johnson <jared@jaredjohnson.dev>
2023-09-28 09:39:32 -0400

    Fix lyric test todos by removing callback

--- tests/unit/musicxml/lyric.test.ts
+++ tests/unit/musicxml/lyric.test.ts
@@ -2,16 +2,16 @@ import { Lyric } from '@/musicxml';
 
 describe(Lyric, () => {
   describe('getVerseNumber', () => {
-    it.todo('returns the verse number of the lyric', () => {});
+    it.todo('returns the verse number of the lyric');
 
-    it.todo('defaults to 1 when the verse number is missing', () => {});
+    it.todo('defaults to 1 when the verse number is missing');
 
-    it.todo('defaults to 1 when the verse number is not a valid integer', () => {});
+    it.todo('defaults to 1 when the verse number is not a valid integer');
   });
 
   describe('getComponents', () => {
-    it.todo('returns the components of the lyric', () => {});
+    it.todo('returns the components of the lyric');
 
-    it.todo('returns an empty array when the lyric is empty', () => {});
+    it.todo('returns an empty array when the lyric is empty');
   });
 });
