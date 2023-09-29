6c293c9c073e828bed3500661e4a7585f47c22c6
Jared Johnson <jared@jaredjohnson.dev>
2023-09-28 21:39:22 -0400

    Add test todos for measurestyle

---
+++ tests/unit/musicxml/measurestyle.test.ts
@@ -0,0 +1,19 @@
+import { MeasureStyle } from '@/musicxml';
+
+describe(MeasureStyle, () => {
+  describe('getStaffNumber', () => {
+    it.todo('returns the number of the measure style');
+
+    it.todo('defaults to 1 when number is missing');
+
+    it.todo('defaults to 1 when number is invalid');
+  });
+
+  describe('getMultipleRestCount', () => {
+    it.todo('returns the multiple rest count');
+
+    it.todo('returns 0 when multiple rest is missing');
+
+    it.todo('returns 0 when multiple rest is invalid');
+  });
+});
