a185206a9957c0db15488ef6cbfa8845ac702823
Jared Johnson <jared@jaredjohnson.dev>
2023-10-01 15:19:37 -0400

    Render <movement-title> when available

--- src/rendering/config.ts
+++ src/rendering/config.ts
@@ -1,13 +1,21 @@
 export type Config = {
   defaultSystemDistance: number;
   defaultStaffDistance: number;
+  titleTopPadding: number;
+  titleFontFamily: string;
+  titleFontSize: string;
   measurePadding: number;
+  measureNumberFontSize: string;
   multiMeasureRestWidth: number;
 };
 
 export const DEFAULT_CONFIG: Config = {
   defaultSystemDistance: 80,
   defaultStaffDistance: 80,
+  titleTopPadding: 40,
+  titleFontFamily: 'Arial',
+  titleFontSize: '36px',
   measurePadding: 80,
+  measureNumberFontSize: '10px',
   multiMeasureRestWidth: 200,
 } as const;
