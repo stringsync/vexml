4acc186959aba751389330863c24865a75a63478
Jared Johnson <jared@jaredjohnson.dev>
2023-10-04 07:44:58 -0400

    Use readline to notify user of resnap caveat

--- scripts/resnap.js
+++ scripts/resnap.js
@@ -1,26 +1,35 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { execSync } = require('child_process');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 try {
-  console.log('Stashing any pending git changes...');
-  execSync('git stash');
+  rl.question(`WARNING: Your staged git changes will become unstaged. Do you want to continue? (y/n): `, (answer) => {
+    if (answer === 'y') {
+      console.log('Stashing any pending git changes...');
+      execSync('git stash');
 
-  console.log('Getting current branch...');
-  const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+      console.log('Getting current branch...');
+      const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
-  console.log('Checking out origin/master...');
-  execSync('git -c advice.detachedHead=false checkout origin/master');
+      console.log('Checking out origin/master...');
+      execSync('git -c advice.detachedHead=false checkout origin/master');
 
-  console.log('Updating snapshots...');
-  execSync('JEST_IMAGE_SNAPSHOT_TRACK_OBSOLETE=1 yarn jest integration --silent --updateSnapshot');
+      console.log('Updating snapshots...');
+      execSync('JEST_IMAGE_SNAPSHOT_TRACK_OBSOLETE=1 yarn jest integration --updateSnapshot > /dev/null');
 
-  console.log(`Checking out ${branch}...`);
-  execSync(`git checkout ${branch}`);
+      console.log(`Checking out ${branch}...`);
+      execSync(`git checkout ${branch}`);
 
-  console.log('Unstashing changes...');
-  execSync('git stash pop');
-
-  console.log('Done.');
+      console.log('Unstashing changes...');
+      execSync('git stash pop');
+    }
+    rl.close();
+  });
 } catch (error) {
   console.error(`Error executing a command: ${error.message}`);
 }
