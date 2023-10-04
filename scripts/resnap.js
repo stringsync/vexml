53f1ad84d3e44e24cfefb4f2ef9fc6fe0d7e849b
Jared Johnson <jared@jaredjohnson.dev>
2023-10-04 07:35:19 -0400

    Simplify resnap script

--- scripts/resnap.js
+++ scripts/resnap.js
@@ -1,27 +1,23 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { execSync } = require('child_process');
 
-// Avoid injection attacks via branch names.
-function shellEscape(str) {
-  return `'${str.replace(/'/g, `'\\''`)}'`;
-}
-
 try {
   console.log('Stashing any pending git changes...');
   execSync('git stash');
 
-  const branch = shellEscape(execSync('git rev-parse --abbrev-ref HEAD').toString().trim());
-  console.log(`Current branch: ${branch}`);
+  console.log('Getting current branch...');
+  const branch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
+  console.log('Checking out origin/master...');
   execSync('git -c advice.detachedHead=false checkout origin/master');
 
   console.log('Updating snapshots...');
   execSync('JEST_IMAGE_SNAPSHOT_TRACK_OBSOLETE=1 yarn jest integration --silent --updateSnapshot');
 
-  console.log(`Checking out ${branch}`);
+  console.log(`Checking out ${branch}...`);
   execSync(`git checkout ${branch}`);
 
-  console.log('Unstashing changes');
+  console.log('Unstashing changes...');
   execSync('git stash pop');
 
   console.log('Done.');
