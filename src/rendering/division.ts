0fe6bef9a84d2cf0677f6adddf0a0a7913bde576
Jared Johnson <jared@jaredjohnson.dev>
2023-10-05 19:15:34 -0400

    Create the Division class

---
+++ src/rendering/division.ts
@@ -0,0 +1,33 @@
+import { Fraction } from '@/util';
+
+/**
+ * Encapsulates the concept of musical timing subdivisions within a measure.
+ *
+ * The `Division` class provides a unified way to manage and manipulate musical divisions, ensuring consistent
+ * representations and operations upon them.
+ */
+export class Division {
+  private constructor(private fraction: Fraction) {}
+
+  /**
+   * Creates a Division.
+   *
+   * @param divisions The number of divisions.
+   * @param quarterNoteDivisions The number of divisions that correspond to a quarter note.
+   */
+  static of(divisions: number, quarterNoteDivisions: number): Division {
+    const fraction = new Fraction(divisions, quarterNoteDivisions);
+    return new Division(fraction);
+  }
+
+  /** Returns the sum as a new Division. */
+  add(value: Division) {
+    const fraction = this.fraction.add(value.fraction);
+    return new Division(fraction);
+  }
+
+  /** Returns the number of beats (quarter notes). */
+  toBeats(): number {
+    return this.fraction.toDecimal();
+  }
+}
