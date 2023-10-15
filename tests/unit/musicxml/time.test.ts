480ea9b62368a0616244a4cd2420adf6ac435dd3
Jared Johnson <jared@jaredjohnson.dev>
2023-10-15 12:27:42 -0400

    Add time tests

--- tests/unit/musicxml/time.test.ts
+++ tests/unit/musicxml/time.test.ts
@@ -1,25 +1,85 @@
 import { Time } from '@/musicxml/time';
+import { xml } from '@/util';
+import { TIME_SYMBOLS } from '@/musicxml';
 
 describe(Time, () => {
   describe('getStaveNumber', () => {
-    it.todo('returns the stave number of the time');
+    it('returns the stave number of the time', () => {
+      const node = xml.time({ staveNumber: 42 });
+      const time = new Time(node);
+      expect(time.getStaveNumber()).toBe(42);
+    });
+
+    it('defaults to 1 when missing', () => {
+      const node = xml.time();
+      const time = new Time(node);
+      expect(time.getStaveNumber()).toBe(1);
+    });
   });
 
   describe('getBeats', () => {
-    it.todo('returns the beats of the time');
+    it('returns the beats of the time', () => {
+      const node = xml.time({
+        times: [{ beats: xml.beats({ value: '3' }) }],
+      });
+      const time = new Time(node);
+      expect(time.getBeats()).toStrictEqual(['3']);
+    });
+
+    it('returns an empty array when beats is missing', () => {
+      const node = xml.time();
+      const time = new Time(node);
+      expect(time.getBeats()).toStrictEqual([]);
+    });
   });
 
   describe('getBeatTypes', () => {
-    it.todo('returns the beat types of the time');
+    it('returns the beat types of the time', () => {
+      const node = xml.time({
+        times: [{ beatType: xml.beatType({ value: '3' }) }],
+      });
+      const time = new Time(node);
+      expect(time.getBeatTypes()).toStrictEqual(['3']);
+    });
+
+    it('returns an empty array when beat types is missing', () => {
+      const node = xml.time();
+      const time = new Time(node);
+      expect(time.getBeatTypes()).toStrictEqual([]);
+    });
   });
 
   describe('isHidden', () => {
-    it.todo('returns true when there is a <senza-misura>');
+    it('returns true when there is a <senza-misura>', () => {
+      const node = xml.time({ senzaMisura: xml.senzaMisura() });
+      const time = new Time(node);
+      expect(time.isHidden()).toBeTrue();
+    });
 
-    it.todo('returns false when there is not a <senza-misura>');
+    it('returns false when there is not a <senza-misura>', () => {
+      const node = xml.time();
+      const time = new Time(node);
+      expect(time.isHidden()).toBeFalse();
+    });
   });
 
   describe('getSymbol', () => {
-    it.todo('returns the symbol of the time');
+    it.each(TIME_SYMBOLS.values)('returns the symbol of the time: %s', (symbol) => {
+      const node = xml.time({ symbol });
+      const time = new Time(node);
+      expect(time.getSymbol()).toBe(symbol);
+    });
+
+    it('defaults to null when missing', () => {
+      const node = xml.time();
+      const time = new Time(node);
+      expect(time.getSymbol()).toBeNull();
+    });
+
+    it('defaults to null when invalid', () => {
+      const node = xml.time({ symbol: 'foo' });
+      const time = new Time(node);
+      expect(time.getSymbol()).toBeNull();
+    });
   });
 });
