b5cdf23b7e0fa9b99649611d36b28033c67a4342
Jared Johnson <jared@jaredjohnson.dev>
2023-10-20 06:41:54 -0400

    Add todo test cases for Direction, DirectionType, and Metronome

---
+++ tests/unit/musicxml/direction.test.ts
@@ -0,0 +1,9 @@
+import { Direction } from '@/musicxml';
+
+describe(Direction, () => {
+  describe('getTypes', () => {
+    it.todo('returns the direction types of the direction');
+
+    it.todo('defaults to an empty array when direction types are missing');
+  });
+});
---
+++ tests/unit/musicxml/directiontype.test.ts
@@ -0,0 +1,11 @@
+import { DirectionType } from '@/musicxml';
+
+describe(DirectionType, () => {
+  describe('getContent', () => {
+    it.todo('returns the metronome when supplied');
+
+    it.todo('returns an empty content when none is provided');
+
+    it.todo('returns an unsupported content when the content is not supported');
+  });
+});
