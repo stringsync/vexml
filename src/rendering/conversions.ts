f5c903d4a56e09d04e540bd290e2871730784f42
Jared Johnson <jared@jaredjohnson.dev>
2023-10-22 11:33:46 -0400

    Move Voice.toDurationDenominator to conversions

--- src/rendering/conversions.ts
+++ src/rendering/conversions.ts
@@ -1,6 +1,8 @@
 import * as musicxml from '@/musicxml';
 import { AccidentalCode } from './accidental';
+import { NoteDurationDenominator } from './enums';
 
+/** Converts an `AccidentalType` and an `Alter` to an accidental code. Defaults to 'n'. */
 export const toAccidentalCode = (opts: {
   accidentalType: musicxml.AccidentalType | null;
   alter: number | null;
@@ -42,4 +44,40 @@ export const toAccidentalCode = (opts: {
   }
 
   return 'n';
+};
+
+/** Converts a `NoteType` to a `NoteDurationDenominator`. Defaults to null. */
+export const toNoteDurationDenominator = (noteType: musicxml.NoteType | null): NoteDurationDenominator | null => {
+  switch (noteType) {
+    case '1024th':
+      return '1024';
+    case '512th':
+      return '512';
+    case '256th':
+      return '256';
+    case '128th':
+      return '128';
+    case '64th':
+      return '64';
+    case '32nd':
+      return '32';
+    case '16th':
+      return '16';
+    case 'eighth':
+      return '8';
+    case 'quarter':
+      return '4';
+    case 'half':
+      return '2';
+    case 'whole':
+      return '1';
+    case 'breve':
+      return '1/2';
+    case 'long':
+      // VexFlow bug: should be '1/4' but it is not supported
+      // return '1/4';
+      return '1/2';
+    default:
+      return null;
+  }
 };
