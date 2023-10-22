bf6141f4d604082b9ce2c554d089694e6bc76574
Jared Johnson <jared@jaredjohnson.dev>
2023-10-22 08:31:18 -0400

    Prototype seed class

--- src/util/array.ts
+++ src/util/array.ts
@@ -32,12 +32,16 @@ export const sortBy = <T, S>(array: T[], transform: (item: T) => S): T[] => {
 /** Iterates over each [previous, current, next] triple. */
 export const forEachTriple = <T>(
   array: T[],
-  callback: (triple: [previous: T | null, current: T, next: T | null], index: number) => void
+  callback: (
+    triple: [previous: T | null, current: T, next: T | null],
+    index: number,
+    meta: { isFirst: boolean; isLast: boolean }
+  ) => void
 ): void => {
   for (let index = 0; index < array.length; index++) {
     const previous = array[index - 1] ?? null;
     const current = array[index];
     const next = array[index + 1] ?? null;
-    callback([previous, current, next], index);
+    callback([previous, current, next], index, { isFirst: index === 0, isLast: index === array.length - 1 });
   }
 };
