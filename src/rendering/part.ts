600810983a467aeb030eebeae17f014c74d1645b
Jared Johnson <jared@jaredjohnson.dev>
2023-10-25 20:39:14 -0400

    Scaffold new Part

---
+++ src/rendering/part.ts
@@ -0,0 +1,102 @@
+import * as musicxml from '@/musicxml';
+import * as vexflow from 'vexflow';
+import * as util from '@/util';
+import { Measure, MeasureRendering } from './measure';
+import { PartRendering } from './legacypart';
+import { StaveSignature } from './stavesignature';
+
+const STAVE_CONNECTOR_BRACE_WIDTH = 16;
+
+/**
+ * Represents a Part in a musical score, corresponding to the <part> element in MusicXML. This class encompasses the
+ * entire musical content for a specific instrument or voice, potentially spanning multiple systems when rendered in the
+ * viewport.
+ */
+export class Part {
+  private musicXml: { part: musicxml.Part };
+  private measures: Measure[];
+
+  constructor(opts: { musicXml: { part: musicxml.Part }; measures: Measure[] }) {
+    this.musicXml = opts.musicXml;
+    this.measures = opts.measures;
+  }
+
+  render(opts: {
+    x: number;
+    y: number;
+    targetSystemWidth: number;
+    minRequiredSystemWidth: number;
+    isLastSystem: boolean;
+    previousPart: Part | null;
+    nextPart: Part | null;
+  }): PartRendering {
+    const measureRenderings = new Array<MeasureRendering>();
+
+    let x = opts.x;
+    const y = opts.y + this.getTopPadding();
+
+    let vfStaveConnector: vexflow.StaveConnector | null = null;
+
+    util.forEachTriple(this.measures, ([previousMeasure, currentMeasure, nextMeasure], index) => {
+      if (index === 0) {
+        previousMeasure = util.last(opts.previousPart?.measures ?? []);
+      }
+      if (index === this.measures.length - 1) {
+        nextMeasure = util.first(opts.nextPart?.measures ?? []);
+      }
+
+      const hasStaveConnectorBrace = index === 0 && this.getStaveCount() > 1;
+
+      if (hasStaveConnectorBrace) {
+        x += STAVE_CONNECTOR_BRACE_WIDTH;
+      }
+
+      const measureRendering = currentMeasure.render({
+        x,
+        y,
+        isLastSystem: opts.isLastSystem,
+        minRequiredSystemWidth: opts.minRequiredSystemWidth,
+        targetSystemWidth: opts.targetSystemWidth,
+        previousMeasure,
+        nextMeasure,
+      });
+      measureRenderings.push(measureRendering);
+
+      const staves = measureRendering.fragments.flatMap((fragment) => fragment.staves);
+      if (hasStaveConnectorBrace) {
+        const topStave = util.first(staves)!;
+        const bottomStave = util.last(staves)!;
+
+        vfStaveConnector = new vexflow.StaveConnector(topStave.vexflow.stave, bottomStave.vexflow.stave).setType(
+          'brace'
+        );
+      }
+
+      x += measureRendering.width;
+    });
+
+    return {
+      id: this.musicXml.part.getId(),
+      vexflow: { staveConnector: vfStaveConnector },
+      measures: measureRenderings,
+    };
+  }
+
+  @util.memoize()
+  private getMeasureEntryGroups() {
+    return StaveSignature.toMeasureEntryGroups({ part: this.musicXml.part });
+  }
+
+  private getTopPadding(): number {
+    return util.max(this.measures.map((measure) => measure.getTopPadding()));
+  }
+
+  private getStaveCount(): number {
+    return util.max(
+      this.getMeasureEntryGroups()
+        .flat()
+        .filter((entry): entry is StaveSignature => entry instanceof StaveSignature)
+        .map((entry) => entry.getStaveCount())
+    );
+  }
+}
