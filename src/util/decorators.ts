c8df45e47a18f4cbdcfd4434c7427b02e6110f92
Jared Johnson <jared@jaredjohnson.dev>
2023-10-28 10:05:30 -0400

    Upgrade memoize to handle args

--- src/util/decorators.ts
+++ src/util/decorators.ts
@@ -1,25 +1,67 @@
-export function memoize(): MethodDecorator {
+import { LRU } from './lru';
+
+/**
+ * Memoizes a method.
+ *
+ * This decorator is backed by an LRU cache to avoid memory leaks. Each argument provided can yield another LRU true if
+ * there are arguments after it. The `degree` parameter is the capacity of each LRU cache "level". It must be declared
+ * statically.
+ */
+export function memoize(opts: { degree: number } = { degree: 1 }): MethodDecorator {
   // eslint-disable-next-line @typescript-eslint/ban-types
   return function (target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>) {
     const originalMethod = descriptor.value;
 
     // Using symbols to ensure uniqueness and avoid potential property name collisions
-    const isCachedSymbol = Symbol(`isCached_${String(propertyKey)}`);
-    const cachedResultSymbol = Symbol(`cachedResult_${String(propertyKey)}`);
+    const cacheSymbol = Symbol(`cache_${String(propertyKey)}`);
 
     descriptor.value = function (...args: any[]) {
-      if (args.length > 0) {
-        throw new Error(`cannot memoize a function that takes arguments`);
-      }
-
       // Sidestep TypeScript typings.
       const self = this as any;
 
-      if (!self[isCachedSymbol]) {
-        self[cachedResultSymbol] = originalMethod.call(self);
-        self[isCachedSymbol] = true;
+      self[cacheSymbol] ??= new LRU(opts.degree);
+
+      let node: LRU<any, any> = self[cacheSymbol];
+      const depth = args.length;
+
+      // When there are no args, we don't need to traverse the LRU tree. However, we do need to store a key in the root
+      // map in case the function is variadic.
+      if (depth === 0) {
+        if (!node.has(depth)) {
+          node.put(depth, originalMethod.apply(self, args));
+        }
+        return node.get(depth);
       }
-      return self[cachedResultSymbol];
+
+      // We need to traverse the LRU tree, starting with the argument length as a namespace. This takes up more space,
+      // but it prevents unintentional exposure of the underlying LRU caches.
+      let value;
+
+      if (!node.has(depth)) {
+        node.put(depth, new LRU(opts.degree));
+      }
+      node = node.get(depth);
+
+      for (let index = 0; index < depth; index++) {
+        const arg = args[index];
+        const isLast = index === depth - 1;
+
+        if (isLast) {
+          // We're on the last arg, so now is the time to check the value.
+          if (!node.has(arg)) {
+            node.put(arg, originalMethod.apply(self, args));
+          }
+          value = node.get(arg);
+        } else {
+          // We still have other args, so we'll create a new LRU to memoize them.
+          if (!node.has(arg)) {
+            node.put(arg, new LRU(opts.degree));
+          }
+          node = node.get(arg);
+        }
+      }
+
+      return value;
     };
 
     return descriptor;
--- tests/unit/util/decorators.test.ts
+++ tests/unit/util/decorators.test.ts
@@ -2,43 +2,188 @@ import * as util from '@/util';
 
 describe('decorators', () => {
   describe('memoize', () => {
-    class Foo {
-      private callCount: number;
+    describe('on a method with no args', () => {
+      class NoArgs {
+        private callCount: number;
 
-      constructor(private value: symbol) {
-        this.callCount = 0;
+        constructor() {
+          this.callCount = 0;
+        }
+
+        @util.memoize()
+        echo(value: symbol): symbol {
+          this.callCount++;
+          return value;
+        }
+
+        getNumCalls(): number {
+          return this.callCount;
+        }
       }
 
-      @util.memoize()
-      getValue(): symbol {
-        this.callCount++;
-        return this.value;
-      }
+      it('memoizes the method', () => {
+        const value = Symbol();
 
-      getNumCalls(): number {
-        return this.callCount;
-      }
-    }
+        const instance = new NoArgs();
 
-    it('memoizes the method', () => {
-      const value = Symbol();
+        expect(instance.echo(value)).toBe(value);
+        expect(instance.echo(value)).toBe(value);
+        expect(instance.getNumCalls()).toBe(1);
+      });
 
-      const foo = new Foo(value);
+      it('has a unique memo per instance', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
 
-      expect(foo.getValue()).toBe(value);
-      expect(foo.getValue()).toBe(value);
-      expect(foo.getNumCalls()).toBe(1);
+        const instance1 = new NoArgs();
+        const instance2 = new NoArgs();
+
+        expect(instance1.echo(value1)).toBe(value1);
+        expect(instance2.echo(value2)).toBe(value2);
+      });
     });
 
-    it('has a unique memo per instance', () => {
-      const value1 = Symbol('1');
-      const value2 = Symbol('2');
+    describe('on a method with a single arg', () => {
+      class SingleArg {
+        private callCount: number;
 
-      const foo1 = new Foo(value1);
-      const foo2 = new Foo(value2);
+        constructor() {
+          this.callCount = 0;
+        }
 
-      expect(foo1.getValue()).toBe(value1);
-      expect(foo2.getValue()).toBe(value2);
+        @util.memoize({ degree: 2 })
+        echo(value: symbol): symbol {
+          this.callCount++;
+          return value;
+        }
+
+        getNumCalls(): number {
+          return this.callCount;
+        }
+      }
+
+      it('memoizes the method', () => {
+        const value = Symbol('1');
+
+        const instance = new SingleArg();
+
+        expect(instance.echo(value)).toBe(value);
+        expect(instance.echo(value)).toBe(value);
+        expect(instance.getNumCalls()).toBe(1);
+      });
+
+      it('memoizes the method with multiple values', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
+
+        const instance = new SingleArg();
+
+        expect(instance.echo(value1)).toBe(value1);
+        expect(instance.echo(value2)).toBe(value2);
+
+        expect(instance.echo(value1)).toBe(value1);
+        expect(instance.echo(value2)).toBe(value2);
+
+        expect(instance.getNumCalls()).toBe(2);
+      });
+
+      it('has a unique memo per instance', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
+
+        const instance1 = new SingleArg();
+        const instance2 = new SingleArg();
+
+        expect(instance1.echo(value1)).toBe(value1);
+        expect(instance2.echo(value2)).toBe(value2);
+      });
+
+      it('overwrites the memo when at capacity', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
+        const value3 = Symbol('3');
+
+        const instance = new SingleArg();
+
+        expect(instance.echo(value1)).toBe(value1);
+        expect(instance.echo(value2)).toBe(value2);
+        expect(instance.echo(value3)).toBe(value3);
+
+        expect(instance.echo(value1)).toBe(value1);
+        expect(instance.echo(value2)).toBe(value2);
+        expect(instance.echo(value3)).toBe(value3);
+
+        expect(instance.getNumCalls()).toBe(6);
+      });
+    });
+
+    describe('on a method with a multiple args', () => {
+      class MultiArgs {
+        private callCount: number;
+
+        constructor() {
+          this.callCount = 0;
+        }
+
+        @util.memoize()
+        echo(value1: symbol, value2: symbol): symbol[] {
+          this.callCount++;
+          return [value1, value2];
+        }
+
+        getNumCalls(): number {
+          return this.callCount;
+        }
+      }
+
+      it('memoizes the method', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
+
+        const instance = new MultiArgs();
+
+        expect(instance.echo(value1, value2)).toStrictEqual([value1, value2]);
+        expect(instance.echo(value1, value2)).toStrictEqual([value1, value2]);
+        expect(instance.getNumCalls()).toBe(1);
+      });
+    });
+
+    describe('on a method with variadic args', () => {
+      class MultiArgs {
+        private callCount: number;
+
+        constructor() {
+          this.callCount = 0;
+        }
+
+        @util.memoize({ degree: 2 })
+        echo(...values: symbol[]): symbol[] {
+          this.callCount++;
+          return values;
+        }
+
+        getNumCalls(): number {
+          return this.callCount;
+        }
+      }
+
+      it('memoizes the method', () => {
+        const value1 = Symbol('1');
+        const value2 = Symbol('2');
+
+        const instance = new MultiArgs();
+
+        expect(instance.echo(value1, value2)).toStrictEqual([value1, value2]);
+        expect(instance.echo(value1, value2)).toStrictEqual([value1, value2]);
+
+        expect(instance.echo(value1)).toStrictEqual([value1]);
+        expect(instance.echo(value1)).toStrictEqual([value1]);
+
+        expect(instance.echo(value1, value2)).toStrictEqual([value1, value2]);
+        expect(instance.echo(value1)).toStrictEqual([value1]);
+
+        expect(instance.getNumCalls()).toBe(2);
+      });
     });
   });
 });
