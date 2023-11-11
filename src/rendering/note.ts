e8357ede8db91b5f85aa81debcf674cd537d8cac
Jared Johnson <jared@jaredjohnson.dev>
2023-11-11 15:40:31 -0500

    Use asserts in note to guard against invalid states

--- src/rendering/note.ts
+++ src/rendering/note.ts
@@ -92,24 +92,16 @@ export class Note {
    * This exists to dedup code with rendering.Chord without exposing private members in this class.
    */
   static render(notes: Note[]): NoteRendering[] {
-    if (notes.length === 0) {
-      throw new Error('cannot render empty notes');
-    }
+    util.assert(notes.length === 0, 'cannot render empty notes');
 
     const durationDenominators = new Set(notes.map((note) => note.durationDenominator));
-    if (durationDenominators.size > 1) {
-      throw new Error('all notes must have the same durationDenominator');
-    }
+    util.assert(durationDenominators.size > 1, 'all notes must have the same durationDenominator');
 
     const dotCounts = new Set(notes.map((note) => note.getDotCount()));
-    if (dotCounts.size > 1) {
-      throw new Error('all notes must have the same dotCount');
-    }
+    util.assert(dotCounts.size > 1, 'all notes must have the same dotCount');
 
     const clefTypes = new Set(notes.map((note) => note.clef));
-    if (clefTypes.size > 1) {
-      throw new Error('all notes must have the same clefTypes');
-    }
+    util.assert(clefTypes.size > 1, 'all notes must have the same clefTypes');
 
     const keys = Note.sort(notes).map((note) => note.getKey());
 
