58f395a330d6f81040b43bcc98270cb418638c89
Jared Johnson <jared@jaredjohnson.dev>
2023-11-12 07:46:47 -0500

    Implement wavy line rendering

--- src/rendering/wavyline.ts
+++ src/rendering/wavyline.ts
@@ -1,4 +1,5 @@
 import * as vexflow from 'vexflow';
+import { SpannerFragmentPhase } from './enums';
 
 /** The result of rendering a wavy line. */
 export type WavyLineRendering = {
@@ -8,17 +9,31 @@ export type WavyLineRendering = {
   };
 };
 
+/** A piece of a wavy line. */
+export type WavyLineFragment = {
+  type: 'wavyline';
+  phase: SpannerFragmentPhase;
+};
+
 /**
  * Wavy lines are one way to indicate trills and vibrato.
  *
  * See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/wavy-line/
  */
 export class WavyLine {
+  private fragments: WavyLineFragment[];
+
+  constructor(opts: { fragments: WavyLineFragment[] }) {
+    this.fragments = opts.fragments;
+  }
+
   render(): WavyLineRendering {
+    const vfOrnaments = this.fragments.map(() => new vexflow.Ornament('prallprall'));
+
     return {
       type: 'wavyline',
       vexflow: {
-        ornaments: [],
+        ornaments: vfOrnaments,
       },
     };
   }
