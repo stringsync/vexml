89067843e8ae605f6ae7be5a3dc8bb455aac8489
Jared Johnson <jared@jaredjohnson.dev>
2023-11-11 20:07:40 -0500

    Prototype rendering.OctaveShift

---
+++ src/rendering/octaveshift.ts
@@ -0,0 +1,67 @@
+import * as vexflow from 'vexflow';
+import * as util from '@/util';
+import { SpannerFragmentPhase } from './enums';
+
+/** The result of rendering an octive shift. */
+export type OctaveShiftRendering = {
+  type: 'octaveshift';
+  vexflow: {
+    textBracket: vexflow.TextBracket;
+  };
+};
+
+/** A piece of an octave shift. */
+export type OctaveShiftFragment = {
+  type: 'octaveshift';
+  phase: SpannerFragmentPhase;
+  vexflow: {
+    note: vexflow.Note;
+  };
+};
+
+/**
+ * Represents an octave shift.
+ *
+ * See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/octave-shift/
+ */
+export class OctaveShift {
+  private text: string;
+  private superscript: string;
+  private position: vexflow.TextBracketPosition;
+  private fragments: OctaveShiftFragment[];
+
+  constructor(opts: {
+    text: string;
+    superscript: string;
+    position: vexflow.TextBracketPosition;
+    fragments: OctaveShiftFragment[];
+  }) {
+    util.assert(opts.fragments.length >= 2, 'must have at least 2 octave shift fragments');
+
+    this.text = opts.text;
+    this.superscript = opts.superscript;
+    this.position = opts.position;
+    this.fragments = opts.fragments;
+  }
+
+  /** Renders the octave shift. */
+  render(): OctaveShiftRendering {
+    const startNote = util.first(this.fragments)!.vexflow.note;
+    const stopNote = util.last(this.fragments)!.vexflow.note;
+
+    const vfTextBracket = new vexflow.TextBracket({
+      start: startNote,
+      stop: stopNote,
+      position: this.position,
+      text: this.text,
+      superscript: this.superscript,
+    });
+
+    return {
+      type: 'octaveshift',
+      vexflow: {
+        textBracket: vfTextBracket,
+      },
+    };
+  }
+}
