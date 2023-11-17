b6b077f653e93362a778cfeec2b54377027d8a69
Jared Johnson <jared@jaredjohnson.dev>
2023-11-17 14:00:52 -0500

    Convert config to CommonJS

--- globalSetup.js
+++ globalSetup.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 // Adapted from https://jestjs.io/docs/puppeteer.
 
-const { mkdir, writeFile } = require('fs').promises;
-const os = require('os');
-const path = require('path');
-const puppeteer = require('puppeteer');
+import { promises } from 'fs';
+import os from 'os';
+import path from 'path';
+import puppeteer from 'puppeteer';
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
-module.exports = async function () {
+export default async function () {
   const browser = await puppeteer.launch({
     headless: 'new',
     // Required for Docker version of Puppeteer
@@ -19,6 +19,6 @@ module.exports = async function () {
   globalThis.__BROWSER_GLOBAL__ = browser;
 
   // use the file system to expose the wsEndpoint for TestEnvironments
-  await mkdir(DIR, { recursive: true });
-  await writeFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
-};
+  await promises.mkdir(DIR, { recursive: true });
+  await promises.writeFile(path.join(DIR, 'wsEndpoint'), browser.wsEndpoint());
+}
