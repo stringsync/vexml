29a761f45ec3140108fc8880686d9c0aa4751374
Jared Johnson <jared@jaredjohnson.dev>
2023-11-17 14:51:01 -0500

    Contrive a change to trigger workflows

--- site/src/main.ts
+++ site/src/main.ts
@@ -16,7 +16,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite and TypeScript logos to learn more!
     </p>
   </div>
 `;
