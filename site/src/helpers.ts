e0ae2325f50d8cbd25208ff16dc97f742d6c2f0b
Jared Johnson <jared@jaredjohnson.dev>
2023-11-18 09:49:01 -0500

    Load the correct version of Bootstrap and fix markup

--- site/src/helpers.ts
+++ site/src/helpers.ts
@@ -2,10 +2,23 @@ type Ctor<T> = {
   new (): T;
 };
 
-export function $<T extends HTMLElement>(id: string, type: Ctor<T>): T {
+export function getById<T extends HTMLElement>(id: string, type: Ctor<T>): T {
   const element = document.getElementById(id);
   if (element instanceof type) {
     return element;
   }
   throw new Error(`expected #${id} to be '${type}', got: '${element}'`);
+}
+
+export function debounce<F extends (...args: any[]) => void>(callback: F, waitFor: number): F {
+  let timeout: NodeJS.Timeout | undefined;
+
+  const debounced = (...args: Parameters<F>) => {
+    if (timeout !== undefined) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => callback(...args), waitFor);
+  };
+
+  return debounced as F;
 }
