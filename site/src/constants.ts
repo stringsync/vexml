1e86e7b1711153af268df4a93e79b414d8953901
Jared Johnson <jared@jaredjohnson.dev>
2023-11-19 20:06:54 -0500

    Show package.json version in site

--- site/src/constants.ts
+++ site/src/constants.ts
@@ -1,3 +1,5 @@
+export const VEXML_VERSION = VITE_VEXML_VERSION;
+
 export const LOCAL_STORAGE_SAVED_MUSICXML_KEY = 'vexml:saved_musicxml';
 export const LOCAL_STORAGE_USE_DEFAULT_MUSICXML_KEY = 'vexml:use_default_musicxml';
 
