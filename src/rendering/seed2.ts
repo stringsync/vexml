1a914def8c55e0a2958d756873e73a380bfc3856
Jared Johnson <jared@jaredjohnson.dev>
2023-12-18 09:07:14 -0500

    Implement partial measure creation in seed2

--- src/rendering/seed2.ts
+++ src/rendering/seed2.ts
@@ -1,6 +1,9 @@
 import { System } from './system';
 import { Config } from './config';
 import * as musicxml from '@/musicxml';
+import * as util from '@/util';
+import { PartMap } from './types';
+import { Measure } from './measure2';
 
 /** A reusable data container that houses rendering data to spawn `System` objects. */
 export class Seed {
@@ -25,5 +28,47 @@ export class Seed {
 
   split(width: number): System[] {
     return [];
+  }
+
+  private getMeasures(): Measure[] {
+    const measures = new Array<Measure>();
+
+    const measureCount = this.getMeasureCount();
+
+    for (let measureIndex = 0; measureIndex < measureCount; measureIndex++) {
+      measures.push(
+        new Measure({
+          config: this.config,
+          index: measureIndex,
+          partIds: this.getPartIds(),
+          musicXml: {
+            measure: this.getMeasurePartMap(measureIndex),
+            staveLayouts: this.musicXml.staveLayouts,
+          },
+        })
+      );
+    }
+
+    return measures;
+  }
+
+  private getMeasurePartMap(measureIndex: number): PartMap<musicxml.Measure> {
+    const result: PartMap<musicxml.Measure> = {};
+
+    for (const part of this.musicXml.parts) {
+      const partId = part.getId();
+      const measures = part.getMeasures();
+      result[partId] = measures[measureIndex];
+    }
+
+    return result;
+  }
+
+  private getMeasureCount(): number {
+    return util.max(this.musicXml.parts.map((part) => part.getMeasures().length));
+  }
+
+  private getPartIds(): string[] {
+    throw this.musicXml.parts.map((part) => part.getId());
   }
 }
