741e49638903f6f1c2422ce2ac0bb16a72882d9f
Jared Johnson <jared@jaredjohnson.dev>
2023-12-27 16:37:38 -0500

    Render multi_stave_single_part_formatting.musicxml

--- tests/integration/vexml.test.ts
+++ tests/integration/vexml.test.ts
@@ -24,6 +24,7 @@ describe('vexml', () => {
 
   it.each<TestCase>([
     { filename: 'multi_system_spanners.musicxml', width: 400 },
+    { filename: 'multi_stave_single_part_formatting.musicxml', width: 900 },
     // format hint
   ])(`$filename ($width px)`, async (t) => {
     const { document, vexmlDiv, screenshotElementSelector } = setup();
