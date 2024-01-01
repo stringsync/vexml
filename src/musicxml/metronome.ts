297f8beab82fd0d9629528ae131ec6ee9a694088
Jared Johnson <jared@jaredjohnson.dev>
2024-01-01 13:48:07 -0500

    Add support for fully specified metronome marks

--- src/musicxml/metronome.ts
+++ src/musicxml/metronome.ts
@@ -1,6 +1,24 @@
 import { NamedElement } from '../util';
 import { NOTE_TYPES, NoteType } from './enums';
 
+/** The details of a metronome mark. */
+export type MetronomeMark = {
+  left: MetronomeOperand;
+  right: MetronomeOperand;
+};
+
+/** Part of a metronome mark. */
+export type MetronomeOperand =
+  | {
+      type: 'note';
+      unit: NoteType;
+      dotCount: number;
+    }
+  | {
+      type: 'bpm';
+      bpm: number;
+    };
+
 /**
  * Represents metronome marks and other metric relationships.
  *
@@ -10,6 +28,58 @@ import { NOTE_TYPES, NoteType } from './enums';
  */
 export class Metronome {
   constructor(private element: NamedElement<'metronome'>) {}
+
+  /** Returns whether the metronome mark is in parentheses. */
+  parentheses(): boolean {
+    return this.element.attr('parentheses').withDefault('no').str() === 'yes';
+  }
+
+  /** Returns the metronome mark details. Defaults to a quarter note = 120 bpm. */
+  getMark(): MetronomeMark | null {
+    const leftElements = new Array<NamedElement<'beat-unit' | 'beat-unit-dot'>>();
+    const rightElements = new Array<NamedElement<'beat-unit' | 'beat-unit-dot' | 'per-minute'>>();
+
+    for (const child of this.element.children()) {
+      const hasLeftElements = leftElements.length > 0;
+      const hasRightElements = rightElements.length > 0;
+
+      if (child.isNamed('beat-unit')) {
+        if (hasLeftElements) {
+          rightElements.push(child);
+        } else {
+          leftElements.push(child);
+        }
+      }
+
+      if (child.isNamed('beat-unit-dot')) {
+        if (hasRightElements) {
+          rightElements.push(child);
+        } else {
+          leftElements.push(child);
+        }
+      }
+
+      if (child.isNamed('per-minute')) {
+        rightElements.push(child);
+      }
+    }
+
+    const leftOperandType = this.isWellFormedNote(leftElements) ? 'note' : 'invalid';
+    const rightOperandType = this.isWellFormedNote(rightElements)
+      ? 'note'
+      : this.isWellFormedBpm(rightElements)
+      ? 'bpm'
+      : 'invalid';
+
+    if (leftOperandType === 'invalid' || rightOperandType === 'invalid') {
+      return null;
+    }
+
+    return {
+      left: this.operand(leftOperandType, leftElements),
+      right: this.operand(rightOperandType, rightElements),
+    };
+  }
 
   /** Returns the first beat unit. Defaults to 'quarter'. */
   getBeatUnit(): NoteType {
@@ -45,5 +115,40 @@ export class Metronome {
    */
   getBeatsPerMinute(): number | null {
     return this.element.first('per-minute')?.content().int() ?? null;
+  }
+
+  private operand(type: MetronomeOperand['type'], elements: NamedElement<string>[]): MetronomeOperand {
+    switch (type) {
+      case 'note':
+        const unit = elements[0].content().enum(NOTE_TYPES) ?? 'quarter';
+        const dotCount = elements.slice(1).filter((child) => child.isNamed('beat-unit-dot')).length;
+        return { type, unit, dotCount };
+      case 'bpm':
+        const bpm = elements[0].content().int() ?? 120;
+        return { type, bpm };
+    }
+  }
+
+  private isWellFormedNote(elements: NamedElement<string>[]): boolean {
+    if (elements.length === 0) {
+      return false;
+    }
+    if (!elements[0].isNamed('beat-unit')) {
+      return false;
+    }
+    if (elements.slice(1).some((child) => !child.isNamed('beat-unit-dot'))) {
+      return false;
+    }
+    return true;
+  }
+
+  private isWellFormedBpm(elements: NamedElement<string>[]): boolean {
+    if (elements.length > 1) {
+      return false;
+    }
+    if (!elements[0].isNamed('per-minute')) {
+      return false;
+    }
+    return true;
   }
 }
--- tests/unit/musicxml/metronome.test.ts
+++ tests/unit/musicxml/metronome.test.ts
@@ -2,6 +2,157 @@ import { Metronome } from '@/musicxml';
 import { xml } from '@/util';
 
 describe(Metronome, () => {
+  describe('parentheses', () => {
+    it('returns true when the parentheses attribute is yes', () => {
+      const node = xml.metronome({ parentheses: 'yes' });
+      const metronome = new Metronome(node);
+      expect(metronome.parentheses()).toBeTrue();
+    });
+
+    it('returns false when the parentheses attribute is no', () => {
+      const node = xml.metronome({ parentheses: 'no' });
+      const metronome = new Metronome(node);
+      expect(metronome.parentheses()).toBeFalse();
+    });
+
+    it('returns false when the parentheses attribute is missing', () => {
+      const node = xml.metronome();
+      const metronome = new Metronome(node);
+      expect(metronome.parentheses()).toBeFalse();
+    });
+
+    it('returns false when the parentheses attribute is invalid', () => {
+      const node = xml.metronome({ parentheses: 'foo' });
+      const metronome = new Metronome(node);
+      expect(metronome.parentheses()).toBeFalse();
+    });
+  });
+
+  describe('getMark', () => {
+    it('returns a note / bpm metronome mark', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [xml.beatUnit({ noteTypeValue: 'quarter' }), xml.perMinute({ value: '42' })],
+        })
+      );
+
+      expect(metronome.getMark()).toEqual({
+        left: { type: 'note', unit: 'quarter', dotCount: 0 },
+        right: { type: 'bpm', bpm: 42 },
+      });
+    });
+
+    it('returns a note / note metronome mark', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [xml.beatUnit({ noteTypeValue: 'quarter' }), xml.beatUnit({ noteTypeValue: 'half' })],
+        })
+      );
+
+      expect(metronome.getMark()).toEqual({
+        left: { type: 'note', unit: 'quarter', dotCount: 0 },
+        right: { type: 'note', unit: 'half', dotCount: 0 },
+      });
+    });
+
+    it('returns a note (dots) / note metronome mark', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnitDot(),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toEqual({
+        left: { type: 'note', unit: 'quarter', dotCount: 1 },
+        right: { type: 'note', unit: 'half', dotCount: 0 },
+      });
+    });
+
+    it('returns a note / note (dots) metronome mark', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+            xml.beatUnitDot(),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toEqual({
+        left: { type: 'note', unit: 'quarter', dotCount: 0 },
+        right: { type: 'note', unit: 'half', dotCount: 1 },
+      });
+    });
+
+    it('returns a note (dots) / note (dots) metronome mark', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnitDot(),
+            xml.beatUnitDot(),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+            xml.beatUnitDot(),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toEqual({
+        left: { type: 'note', unit: 'quarter', dotCount: 2 },
+        right: { type: 'note', unit: 'half', dotCount: 1 },
+      });
+    });
+
+    it('returns null when the left operand is invalid', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.beatUnitDot(),
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toBeNull();
+    });
+
+    it('returns null when the right operand is invalid', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toBeNull();
+    });
+
+    it('returns null when the left and operands are invalid', () => {
+      const metronome = new Metronome(
+        xml.metronome({
+          content: [
+            xml.perMinute({ value: '42' }),
+            xml.beatUnit({ noteTypeValue: 'quarter' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+            xml.beatUnit({ noteTypeValue: 'half' }),
+            xml.perMinute({ value: '120' }),
+          ],
+        })
+      );
+
+      expect(metronome.getMark()).toBeNull();
+    });
+  });
+
   describe('getBeatUnit', () => {
     it('returns the beat unit of the metronome', () => {
       const beatUnit = xml.beatUnit({ noteTypeValue: 'half' });
