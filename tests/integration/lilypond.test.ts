e1be2cccb0417a54fed4f6bfd7cc32ff2484844b
Jared Johnson <jared@jaredjohnson.dev>
2024-01-01 18:08:16 -0500

    Render lilypond23a

--- tests/integration/lilypond.test.ts
+++ tests/integration/lilypond.test.ts
@@ -64,7 +64,7 @@ describe('lilypond', () => {
     // { filename: '22b-Staff-Notestyles.musicxml', width: 900 },
     { filename: '22c-Noteheads-Chords.musicxml', width: 900 },
     { filename: '22d-Parenthesized-Noteheads.musicxml', width: 900 },
-    // { filename: '23a-Tuplets.musicxml', width: 900 },
+    { filename: '23a-Tuplets.musicxml', width: 900 },
     // { filename: '23b-Tuplets-Styles.musicxml', width: 900 },
     // { filename: '23c-Tuplet-Display-NonStandard.musicxml', width: 900 },
     // { filename: '23d-Tuplets-Nested.musicxml', width: 900 },
