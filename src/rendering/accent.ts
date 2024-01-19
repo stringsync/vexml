8a10cad8e9b49a97f5672ec096079edd82b58436
Jared Johnson <jared@jaredjohnson.dev>
2024-01-19 15:13:52 -0500

    Scaffold rendering Accent

--- src/rendering/accent.ts
+++ src/rendering/accent.ts
@@ -1,6 +1,11 @@
+import * as vexflow from 'vexflow';
+
 /** The result of rendering an accent. */
 export type AccentRendering = {
   type: 'accent';
+  vexflow: {
+    articulation: vexflow.Articulation;
+  };
 };
 
 /** Represents an emphasis, stress, or stronger attack placed on a particular note or set of notes or chord. */
@@ -9,6 +14,9 @@ export class Accent {
   render(): AccentRendering {
     return {
       type: 'accent',
+      vexflow: {
+        articulation: new vexflow.Articulation('a>'),
+      },
     };
   }
 }
