94d60c29b2f4a1c86eb9fbfa9d02481d3aa1a3c7
Jared Johnson <jared@jaredjohnson.dev>
2024-01-19 15:48:09 -0500

    Flesh out rendering Accent

--- src/rendering/accent.ts
+++ src/rendering/accent.ts
@@ -1,4 +1,5 @@
 import * as vexflow from 'vexflow';
+import * as musicxml from '@/musicxml';
 
 /** The result of rendering an accent. */
 export type AccentRendering = {
@@ -10,13 +11,30 @@ export type AccentRendering = {
 
 /** Represents an emphasis, stress, or stronger attack placed on a particular note or set of notes or chord. */
 export class Accent {
+  private musicXML: { accent: musicxml.Accent };
+
+  constructor(opts: { musicXML: { accent: musicxml.Accent } }) {
+    this.musicXML = opts.musicXML;
+  }
+
   /** Renders the Accent. */
   render(): AccentRendering {
     return {
       type: 'accent',
       vexflow: {
-        articulation: new vexflow.Articulation('a>'),
+        articulation: this.getVfArticulation(),
       },
     };
+  }
+
+  private getVfArticulation(): vexflow.Articulation {
+    const placement = this.musicXML.accent.getPlacement();
+
+    switch (placement) {
+      case 'above':
+        return new vexflow.Articulation('a>').setPosition(vexflow.Modifier.Position.ABOVE);
+      case 'below':
+        return new vexflow.Articulation('a>').setPosition(vexflow.Modifier.Position.BELOW);
+    }
   }
 }
