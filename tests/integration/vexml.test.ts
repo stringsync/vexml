4a9c718b687ee6deab3ce05e18a5c4cb758d9571
Jared Johnson <jared@jaredjohnson.dev>
2024-02-13 09:08:11 -0500

    Add pelude_no_1_snippet.musicxml test

--- tests/integration/vexml.test.ts
+++ tests/integration/vexml.test.ts
@@ -27,6 +27,7 @@ describe('vexml', () => {
     { filename: 'multi_stave_single_part_formatting.musicxml', width: 900 },
     { filename: 'multi_part_formatting.musicxml', width: 900 },
     { filename: 'complex_formatting.musicxml', width: 900 },
+    { filename: 'prelude_no_1_snippet.musicxml', width: 900 },
   ])(`$filename ($width px)`, async (t) => {
     const { document, vexmlDiv, screenshotElementSelector } = setup();
 
