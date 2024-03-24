0a5f53d91117f3d6ed10c99660c94b71795af5ec
Jared Johnson <jared@jaredjohnson.dev>
2024-03-24 11:02:37 -0400

    Test tab slide rendering

--- tests/integration/vexml.test.ts
+++ tests/integration/vexml.test.ts
@@ -32,6 +32,7 @@ describe('vexml', () => {
     { filename: 'tabs_with_stave.musicxml', width: 900 },
     { filename: 'tabs_slurs.musicxml', width: 900 },
     { filename: 'tabs_natural_harmonics.musicxml', width: 900 },
+    { filename: 'tabs_slides.musicxml', width: 900 },
   ])(`$filename ($width px)`, async (t) => {
     const { document, vexmlDiv, screenshotElementSelector } = setup();
 
