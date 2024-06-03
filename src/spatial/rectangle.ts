1d01d7b690e9403a32db1faeb1b842dcedc62b7e
Jared Johnson <jared@jaredjohnson.dev>
2024-06-03 07:00:40 -0400

    Add origin factory to Rectangle

--- src/spatial/rectangle.ts
+++ src/spatial/rectangle.ts
@@ -13,6 +13,11 @@ export class Rectangle {
     public readonly h: number
   ) {}
 
+  /** Creates a Rectangle at (0, 0) with the specified width and height. */
+  static origin(w: number, h: number) {
+    return new Rectangle(0, 0, w, h);
+  }
+
   /** Checks if a given point is inside the rectangle. */
   contains(point: Point): boolean {
     return point.x >= this.x && point.x <= this.x + this.w && point.y >= this.y && point.y <= this.y + this.h;
