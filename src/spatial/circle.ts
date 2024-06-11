3acb1e31ef280165704ead699b04161eb1cfca61
Jared Johnson <jared@jaredjohnson.dev>
2024-06-11 06:43:01 -0400

    Simplify Rect and Circle signatures

--- src/spatial/circle.ts
+++ src/spatial/circle.ts
@@ -2,20 +2,14 @@ import { Point } from './point';
 import { Shape } from './types';
 
 export class Circle implements Shape {
-  /** The x-coordinate of the center of the circle. */
-  public readonly x: number;
-
-  /** The y-coordinate of the center of the circle. */
-  public readonly y: number;
-
-  /** The radius of the circle. */
-  public readonly r: number;
-
-  constructor(opts: { x: number; y: number; r: number }) {
-    this.x = opts.x;
-    this.y = opts.y;
-    this.r = opts.r;
-  }
+  constructor(
+    /** center x-coordinate */
+    public readonly x: number,
+    /** center y-coordinate */
+    public readonly y: number,
+    /** radius */
+    public readonly r: number
+  ) {}
 
   center(): Point {
     return new Point(this.x, this.y);
--- src/spatial/rect.ts
+++ src/spatial/rect.ts
@@ -3,21 +3,16 @@ import { Shape } from './types';
 
 /** Represents a rectangle in a 2D coordinate system. */
 export class Rect implements Shape {
-  /** Upper-left corner x coordinate */
-  public readonly x: number;
-  /** Upper-left corner y coordinate */
-  public readonly y: number;
-  /** Total width */
-  public readonly w: number;
-  /** Total height */
-  public readonly h: number;
-
-  constructor(opts: { x: number; y: number; w: number; h: number }) {
-    this.x = opts.x;
-    this.y = opts.y;
-    this.w = opts.w;
-    this.h = opts.h;
-  }
+  constructor(
+    /** upper-left corner x-coordinate */
+    public readonly x: number,
+    /** upper-left corner y-coordinate */
+    public readonly y: number,
+    /** total width */
+    public readonly w: number,
+    /** total height */
+    public readonly h: number
+  ) {}
 
   origin(): Point {
     return new Point(this.x, this.y);
