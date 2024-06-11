a29e4b035b55670032b392ac8f551e6234639c0a
Jared Johnson <jared@jaredjohnson.dev>
2024-06-11 07:01:39 -0400

    Reimplement Rect.corners for the new Rect

--- src/spatial/rect.ts
+++ src/spatial/rect.ts
@@ -22,6 +22,15 @@ export class Rect implements Shape {
     return new Point(this.x + this.w / 2, this.y + this.h / 2);
   }
 
+  corners(): [Point, Point, Point, Point] {
+    return [
+      new Point(this.x, this.y),
+      new Point(this.x + this.w, this.y),
+      new Point(this.x + this.w, this.y + this.h),
+      new Point(this.x, this.y + this.h),
+    ];
+  }
+
   contains(point: Point): boolean {
     return point.x >= this.x && point.x <= this.x + this.w && point.y >= this.y && point.y <= this.y + this.h;
   }
