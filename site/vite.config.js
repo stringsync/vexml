8d0880c262b983e1ef9ad096573afd5ace5be074
Jared Johnson <jared@jaredjohnson.dev>
2024-06-24 06:01:26 -0400

    Replace vxflw-early-access with vexflow 5.0.0-alpha.4

--- site/vite.config.js
+++ site/vite.config.js
@@ -13,7 +13,6 @@ export default defineConfig({
     preserveSymlinks: true,
     alias: {
       '@': path.resolve(__dirname, '..', 'src'),
-      vexflow: path.resolve(__dirname, '..', 'node_modules', 'vxflw-early-access'),
     },
   },
 });
