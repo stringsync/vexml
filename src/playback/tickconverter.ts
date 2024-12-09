6e96ddba63229f0c627c6a21b6c5ea60edc4976c
Jared Johnson <jared@jaredjohnson.dev>
2024-12-08 20:11:03 -0500

    Add comment for hardcoded source

--- src/playback/tickconverter.ts
+++ src/playback/tickconverter.ts
@@ -1,5 +1,6 @@
 import { Duration } from './duration';
 
+// Hardcoded from https://github.com/vexflow/vexflow/blob/a9d8a1cebf390c157f4c8fe99f2cab16e7bfcac0/src/tables.ts#L19
 const TICKS_PER_QUARTER_NOTE = 4096;
 
 export class TickConverter {
