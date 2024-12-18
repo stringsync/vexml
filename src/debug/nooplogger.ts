5d3ff8f3efb0835efc089f1b5657b6c85441179a
Jared Johnson <jared@jaredjohnson.dev>
2024-12-17 20:19:44 -0500

    Implement MemoryLogger and NoopLogger

---
+++ src/debug/nooplogger.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Logger } from './types';
+
+export class NoopLogger implements Logger {
+  info(message: string): void {}
+  warn(message: string): void {}
+  error(message: string): void {}
+}
