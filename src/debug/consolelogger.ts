b2219ba51a279c06e003b45f61bcdc5720cd955e
Jared Johnson <jared@jaredjohnson.dev>
2024-12-17 20:13:41 -0500

    Create a simple debug.Logger interface and implement a ConsoleLogger

---
+++ src/debug/consolelogger.ts
@@ -0,0 +1,15 @@
+import { Logger } from './types';
+
+export class ConsoleLogger implements Logger {
+  info(message: string): void {
+    console.log(message);
+  }
+
+  warn(message: string): void {
+    console.warn(message);
+  }
+
+  error(message: string): void {
+    console.error(message);
+  }
+}
