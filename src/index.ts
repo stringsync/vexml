6672c0891b53f05ac80db6fee2cf99937640a798
Jared Johnson <jared@jaredjohnson.dev>
2024-12-21 10:29:51 -0500

    Wire messageMeasures through to Seed and account for them when constructing measures

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './vexml';
 export * from './rendering/events';
-export type { Rendering } from './rendering';
+export { type Rendering, type MessageMeasure } from './rendering';
 export { SimpleCursor } from './components';
 export { CONFIG_SCHEMA, DEFAULT_CONFIG } from './config';
 export type { Config, SchemaDescriptor, SchemaType, SchemaConfig } from './config';
