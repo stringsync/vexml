ac37802a31248802bd30f6c3dbd2feb5df30911e
Jared Johnson <jared@jaredjohnson.dev>
2024-12-26 08:48:16 -0500

    Implement parsing/musicxml helper structures

--- src/parsing/musicxml/keysignature.ts
+++ src/parsing/musicxml/keysignature.ts
@@ -1,27 +1,69 @@
+import * as musicxml from '@/musicxml';
 import { KeyMode } from './enums';
 
 export class KeySignature {
-  static default() {
-    return new KeySignature();
+  constructor(
+    private partId: string,
+    private staveNumber: number,
+    private fifths: number,
+    private previousKeySignature: KeySignature | null,
+    private mode: KeyMode
+  ) {}
+
+  static default(partId: string, staveNumber: number): KeySignature {
+    return new KeySignature(partId, staveNumber, 0, null, 'none');
   }
 
   getPartId(): string {
-    return '';
+    return this.partId;
   }
 
   getStaveNumber(): number {
-    return 1;
+    return this.staveNumber;
   }
 
   getFifths(): number {
-    return 0;
+    return this.fifths;
   }
 
   getPreviousKeySignature(): KeySignature | null {
-    return null;
+    return this.previousKeySignature;
   }
 
   getMode(): KeyMode {
-    return 'none';
+    return this.mode;
+  }
+
+  isEqual(keySignature: KeySignature): boolean {
+    return (
+      this.partId === keySignature.getPartId() &&
+      this.staveNumber === keySignature.getStaveNumber() &&
+      this.isEquivalent(keySignature)
+    );
+  }
+
+  isEquivalent(keySignature: KeySignature): boolean {
+    return (
+      this.fifths === keySignature.getFifths() &&
+      this.mode === keySignature.getMode() &&
+      this.arePreviousKeySignaturesEquivalent(keySignature.previousKeySignature)
+    );
+  }
+
+  merge(musicXML: { key: musicxml.Key }): KeySignature {
+    return new KeySignature(this.partId, this.staveNumber, musicXML.key.getFifthsCount(), this, musicXML.key.getMode());
+  }
+
+  private arePreviousKeySignaturesEquivalent(previousKeySignature: KeySignature | null): boolean {
+    if (!this.previousKeySignature && !previousKeySignature) {
+      return true;
+    }
+    if (!this.previousKeySignature || !previousKeySignature) {
+      return false;
+    }
+    return (
+      this.previousKeySignature.fifths === previousKeySignature.fifths &&
+      this.previousKeySignature.mode === previousKeySignature.mode
+    );
   }
 }
--- src/parsing/musicxml/metronome.ts
+++ src/parsing/musicxml/metronome.ts
@@ -1,5 +1,64 @@
+import * as musicxml from '@/musicxml';
+
 export class Metronome {
-  static default() {
-    return new Metronome();
+  constructor(
+    private opts: {
+      name?: string;
+      parenthesis?: boolean;
+      duration?: string;
+      dots?: number;
+      bpm?: number | string;
+      duration2?: string;
+      dots2?: number;
+    }
+  ) {}
+
+  static default(): Metronome {
+    return new Metronome({ bpm: 120 });
+  }
+
+  getName(): string | undefined {
+    return this.opts.name;
+  }
+
+  getParenthesis(): boolean | undefined {
+    return this.opts.parenthesis;
+  }
+
+  getDuration(): string | undefined {
+    return this.opts.duration;
+  }
+
+  getDots(): number | undefined {
+    return this.opts.dots;
+  }
+
+  getBpm(): number | string | undefined {
+    return this.opts.bpm;
+  }
+
+  getDuration2(): string | undefined {
+    return this.opts.duration2;
+  }
+
+  getDots2(): number | undefined {
+    return this.opts.dots2;
+  }
+
+  isEqual(metronome: Metronome): boolean {
+    return (
+      this.getName() === metronome.getName() &&
+      this.getParenthesis() === metronome.getParenthesis() &&
+      this.getDuration() === metronome.getDuration() &&
+      this.getDots() === metronome.getDots() &&
+      this.getBpm() === metronome.getBpm() &&
+      this.getDuration2() === metronome.getDuration2() &&
+      this.getDots2() === metronome.getDots2()
+    );
+  }
+
+  merge(musicXML: { metronome: musicxml.Metronome; metronomeMark: musicxml.MetronomeMark }): Metronome {
+    // TODO: Implement this.
+    return new Metronome(this.opts);
   }
 }
