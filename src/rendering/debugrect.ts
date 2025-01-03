f2a1d7aaf19eebea3c5cec8ff286f1cf13e668ee
Jared Johnson <jared@jaredjohnson.dev>
2025-01-02 19:07:58 -0500

    Add dimensions to debug rects

--- src/rendering/debugrect.ts
+++ src/rendering/debugrect.ts
@@ -37,19 +37,47 @@ export class DebugRect {
 
     ctx.save();
 
-    const rect = this.rect;
+    const padding: Padding = {};
+    const font: LabelFont = {
+      color: DEFAULT_STYLE_FONT_COLOR,
+      size: '8px',
+      family: 'monospace',
+    };
 
+    // Draw the main label in the bottom left corner.
     if (this.label) {
       const bottomLeft = this.rect.corners()[3];
       const position = new Point(bottomLeft.x + 1, bottomLeft.y - 1);
-      const padding: Padding = {};
-      const font: LabelFont = {
-        color: DEFAULT_STYLE_FONT_COLOR,
-        size: '8px',
-        family: 'monospace',
-      };
       Label.singleLine(this.config, this.log, this.label, position, padding, font).setContext(ctx).draw();
     }
+
+    // Draw the height label on the middle of the left side.
+    const heightLabelPosition = new Point(this.rect.x + 1, this.rect.center().y + 3);
+    const heightLabel = `${Math.round(this.rect.h)}`;
+    Label.singleLine(this.config, this.log, heightLabel, heightLabelPosition, padding, font).setContext(ctx).draw();
+
+    // Draw the width label on the middle of the bottom side.
+    const widthLabelPosition = new Point(this.rect.center().x, this.rect.y + this.rect.h - 1);
+    const widthLabel = `${Math.round(this.rect.w)}`;
+    Label.singleLine(this.config, this.log, widthLabel, widthLabelPosition, padding, font).setContext(ctx).draw();
+
+    // Draw the position label on the top left corner.
+    const positionLabelPosition = new Point(this.rect.x + 1, this.rect.y + 8);
+    const positionLabel = `${Math.round(this.rect.x)},${Math.round(this.rect.y)}`;
+    Label.singleLine(this.config, this.log, positionLabel, positionLabelPosition, padding, font).setContext(ctx).draw();
+
+    this.drawRect(this.rect);
+
+    ctx.restore();
+
+    return this;
+  }
+
+  private drawRect(rect: Rect): void {
+    const ctx = this.ctx;
+    util.assertNotNull(ctx);
+
+    ctx.save();
 
     const stroke = this.style?.stroke ?? DEFAULT_STYLE_STROKE;
     ctx.setStrokeStyle(stroke);
@@ -63,7 +91,5 @@ export class DebugRect {
     ctx.fillRect(rect.x, rect.y, rect.w, rect.h);
 
     ctx.restore();
-
-    return this;
   }
 }
