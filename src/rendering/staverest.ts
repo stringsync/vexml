d4db285718555345e18349159dd1c4e42d1ce9e3
Jared Johnson <jared@jaredjohnson.dev>
2025-01-06 10:06:41 -0500

    Migrate up to 03b

--- src/rendering/staverest.ts
+++ src/rendering/staverest.ts
@@ -16,14 +16,63 @@ export class StaveRest {
   constructor(private config: Config, private log: Logger, private document: Document, private key: VoiceEntryKey) {}
 
   render(): StaveRestRender {
+    const rest = this.document.getRest(this.key);
+
+    const vexflowStaveNote = new vexflow.StaveNote({
+      keys: this.getVexflowStaveKeys(),
+      duration: `${rest.durationType}r`,
+      dots: rest.dotCount,
+      clef: this.document.getStave(this.key).signature.clef.sign,
+      alignCenter: this.shouldAlignCenter(),
+    });
+
+    for (let index = 0; index < rest.dotCount; index++) {
+      vexflow.Dot.buildAndAttach([vexflowStaveNote]);
+    }
+
     return {
       type: 'staverest',
       key: this.key,
-      rect: Rect.empty(),
-      vexflowTickable: new vexflow.StaveNote({
-        keys: ['b/4'],
-        duration: 'w',
-      }),
+      rect: Rect.empty(), // placeholder
+      vexflowTickable: vexflowStaveNote,
     };
+  }
+  private getVexflowStaveKeys(): string[] {
+    const rest = this.document.getRest(this.key);
+
+    const displayPitch = rest.displayPitch;
+    if (displayPitch) {
+      return [`${displayPitch.step}/${displayPitch.octave}`];
+    }
+
+    const clef = this.document.getStave(this.key).signature.clef;
+    if (clef.sign === 'bass') {
+      return ['D/3'];
+    }
+
+    if (rest.durationType === '2') {
+      return ['B/4'];
+    }
+    if (rest.durationType === '1') {
+      return ['D/5'];
+    }
+    return ['B/4'];
+  }
+
+  private shouldAlignCenter(): boolean {
+    const voiceEntryCount = this.document.getVoiceEntryCount(this.key);
+    if (voiceEntryCount > 1) {
+      return false;
+    }
+
+    const rest = this.document.getRest(this.key);
+    if (rest.durationType === '1') {
+      return true;
+    }
+    if (rest.durationType === '2') {
+      return true;
+    }
+
+    return false;
   }
 }
