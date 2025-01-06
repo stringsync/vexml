6436feb2ac498354d0f161261d0e3f468539fce9
Jared Johnson <jared@jaredjohnson.dev>
2025-01-06 10:06:41 -0500

    Migrate up to 03b

--- src/rendering/voice.ts
+++ src/rendering/voice.ts
@@ -8,6 +8,7 @@ import { Rect } from '@/spatial';
 import { StaveNote, StaveNoteRender } from './stavenote';
 import { StaveRest, StaveRestRender } from './staverest';
 import { Fraction } from '@/util';
+import { DurationType } from '@/data/enums';
 
 export type VoiceEntryRender = StaveNoteRender | StaveRestRender;
 
@@ -18,6 +19,21 @@ export type VoiceRender = {
   vexflowVoice: vexflow.Voice;
   entryRenders: VoiceEntryRender[];
 };
+
+const DURATION_TYPE_VALUES: Array<{ type: DurationType; value: Fraction }> = [
+  { type: '1/2', value: new Fraction(2, 1) },
+  { type: '1', value: new Fraction(1, 1) },
+  { type: '2', value: new Fraction(1, 2) },
+  { type: '4', value: new Fraction(1, 4) },
+  { type: '8', value: new Fraction(1, 8) },
+  { type: '16', value: new Fraction(1, 16) },
+  { type: '32', value: new Fraction(1, 32) },
+  { type: '64', value: new Fraction(1, 64) },
+  { type: '128', value: new Fraction(1, 128) },
+  { type: '256', value: new Fraction(1, 256) },
+  { type: '512', value: new Fraction(1, 512) },
+  { type: '1024', value: new Fraction(1, 1024) },
+];
 
 export class Voice {
   constructor(private config: Config, private log: Logger, private document: Document, private key: VoiceKey) {}
@@ -48,9 +64,11 @@ export class Voice {
       const duration = Fraction.fromFractionLike(entry.duration);
 
       if (currentMeasureBeat.isLessThan(measureBeat)) {
-        const vexflowGhostNote = this.renderVexflowGhostNote(measureBeat.subtract(currentMeasureBeat));
+        const beats = measureBeat.subtract(currentMeasureBeat).divide(new Fraction(4));
+        const vexflowGhostNote = this.renderVexflowGhostNote(beats);
         vexflowVoice.addTickable(vexflowGhostNote);
-        // NOTE: We don't need to add this is entryRenders because it's not a real entry.
+        // NOTE: We don't need to add this is entryRenders because it's a vexflow-specific detail and vexml doesn't need
+        // to do anything with it.
       }
       currentMeasureBeat = measureBeat.add(duration);
 
@@ -70,9 +88,16 @@ export class Voice {
     return entryRenders;
   }
 
-  private renderVexflowGhostNote(duration: Fraction): vexflow.GhostNote {
-    return new vexflow.GhostNote({
-      duration: 'q', // TODO: derive from duration
-    });
+  private renderVexflowGhostNote(beatDuration: Fraction): vexflow.GhostNote {
+    let closestDurationType: DurationType = '1/2';
+
+    for (const { type, value } of DURATION_TYPE_VALUES) {
+      if (value.isLessThanOrEqualTo(beatDuration)) {
+        closestDurationType = type;
+        break;
+      }
+    }
+
+    return new vexflow.GhostNote({ duration: closestDurationType });
   }
 }
