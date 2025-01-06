7e38e5774450693b91588c0cfb4d425258117182
Jared Johnson <jared@jaredjohnson.dev>
2025-01-06 06:53:29 -0500

    Move some helper classes to their own file

--- src/rendering/score.ts
+++ src/rendering/score.ts
@@ -7,6 +7,7 @@ import { SystemKey } from './types';
 import { Label } from './label';
 import { Rect } from '@/spatial';
 import { Pen } from './pen';
+import { SystemRenderMover } from './systemrendermover';
 
 export type ScoreRender = {
   type: 'score';
@@ -111,55 +112,5 @@ export class Score {
     }
 
     return systemRenders;
-  }
-}
-
-type Movable = {
-  rect: Rect;
-};
-
-/**
- * After a system is rendered, we may learn there is excess height from its components. This class recursivley moves
- * all the rects by the excess height such that we can honor the SYSTEM_MARGIN_BOTTOM configuration without
- * re-rendering. This is much faster than re-rendering the system at a different position.
- */
-class SystemRenderMover {
-  moveBy(systemRender: SystemRender, dy: number) {
-    const seen = new Set<any>(); // avoid circular references
-
-    const move = (obj: any) => {
-      if (seen.has(obj)) {
-        return;
-      }
-      seen.add(obj);
-      if (this.isMovable(obj)) {
-        obj.rect = obj.rect.translate({ dy });
-      }
-      if (Array.isArray(obj)) {
-        for (const item of obj) {
-          move(item);
-        }
-      } else if (util.isPOJO(obj)) {
-        for (const key in obj) {
-          move(obj[key]);
-        }
-      }
-    };
-    move(systemRender);
-
-    // Before finishing, we move the vexflow staves. Since everything is linked to them, this should complete the move.
-    // Any future supported vexflow object not connected to a stave will need to be moved here.
-    systemRender.measureRenders
-      .flatMap((m) => m.entryRenders)
-      .flatMap((e) => e.partRenders)
-      .flatMap((p) => p.staveRenders)
-      .map((s) => s.vexflowStave)
-      .forEach((s) => {
-        s.setY(s.getY() + dy);
-      });
-  }
-
-  private isMovable(obj: any): obj is Movable {
-    return !!obj && obj.rect instanceof Rect;
   }
 }
