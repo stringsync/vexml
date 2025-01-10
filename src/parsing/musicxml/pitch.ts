72005a0074088022c22d7e936149c21d9b883256
Jared Johnson <jared@jaredjohnson.dev>
2025-01-10 07:10:34 -0500

    Refactor musicxml/parsing note to also parse grace notes

--- src/parsing/musicxml/pitch.ts
+++ src/parsing/musicxml/pitch.ts
@@ -3,6 +3,14 @@ import * as data from '@/data';
 export class Pitch {
   constructor(private step: string, private octave: number) {}
 
+  getStep(): string {
+    return this.step;
+  }
+
+  getOctave(): number {
+    return this.octave;
+  }
+
   parse(): data.Pitch {
     return {
       type: 'pitch',
