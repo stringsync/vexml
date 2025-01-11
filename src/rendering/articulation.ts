b2cb8c07c109c21aeb0517341b1b6d0c773a666b
Jared Johnson <jared@jaredjohnson.dev>
2025-01-11 16:53:37 -0500

    Render about half of supported articulations

--- src/rendering/articulation.ts
+++ src/rendering/articulation.ts
@@ -46,6 +46,54 @@ export class Articulation {
         return [new vexflow.Articulation('a@s')];
       case 'upright-square-fermata':
         return [new vexflow.Articulation('a@l')];
+      case 'inverted-normal-fermata':
+        return [new vexflow.Articulation('a@u')];
+      case 'inverted-angled-fermata':
+        return [new vexflow.Articulation('a@v')];
+      case 'inverted-square-fermata':
+        return [new vexflow.Articulation('a@r')];
+      case 'up-bow':
+        return [new vexflow.Articulation('a|')];
+      case 'down-bow':
+        return [new vexflow.Articulation('am')];
+      case 'harmonic':
+        return [new vexflow.Articulation('ah')];
+      case 'open-string':
+        return [new vexflow.Articulation('ah')];
+      case 'double-tongue':
+        return [new vexflow.Articulation('..')];
+      case 'triple-tongue':
+        return [new vexflow.Articulation('...')];
+      case 'stopped':
+        return [new vexflow.Articulation('a+')];
+      case 'snap-pizzicato':
+        return [new vexflow.Articulation('ao')];
+      case 'tap':
+        return [new vexflow.Annotation('T')];
+      case 'heel':
+        return [new vexflow.Annotation('U')];
+      case 'toe':
+        return [new vexflow.Annotation('^')];
+      case 'accent':
+        return articulation.placement === 'above' ? [new vexflow.Articulation('a>')] : [new vexflow.Articulation('a-')];
+      case 'strong-accent':
+        return [new vexflow.Articulation('a^')];
+      case 'staccato':
+        return [new vexflow.Articulation('a.')];
+      case 'tenuto':
+        return [new vexflow.Articulation('a-')];
+      case 'detached-legato':
+        return [new vexflow.Articulation('a.'), new vexflow.Articulation('a-')];
+      case 'staccatissimo':
+        return [new vexflow.Articulation('av')];
+      case 'scoop':
+        return [new vexflow.Ornament('scoop')];
+      case 'doit':
+        return [new vexflow.Ornament('doit')];
+      case 'falloff':
+        return [new vexflow.Ornament('fall')];
+      case 'breath-mark':
+        return articulation.placement === 'above' ? [new vexflow.Articulation('a>')] : [new vexflow.Articulation('a-')];
       default:
         this.log.warn(`unsupported articulation type`, { type: articulation.articulationType });
         return [];
