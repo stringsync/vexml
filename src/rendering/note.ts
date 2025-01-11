84d0fdae20f79bd9bf4a74306d07991d7fe91eee
Jared Johnson <jared@jaredjohnson.dev>
2025-01-11 09:39:06 -0500

    Render octave shift spanner

--- src/rendering/note.ts
+++ src/rendering/note.ts
@@ -72,6 +72,7 @@ export class Note {
       graceBeamRenders,
       graceCurves,
       pedalMark: voiceEntry.pedalMark,
+      octaveShiftId: voiceEntry.octaveShiftId,
     };
   }
 
---
+++ src/rendering/octaveshift.ts
@@ -0,0 +1,89 @@
+import * as vexflow from 'vexflow';
+import * as util from '@/util';
+import { Config } from './config';
+import { Logger } from '@/debug';
+import { Rect } from '@/spatial';
+import { Document } from './document';
+import { NoteRender, OctaveShiftKey, OctaveShiftRender } from './types';
+
+interface NoteRenderRegistry {
+  get(octaveShiftId: string): NoteRender[] | undefined;
+}
+
+export class OctaveShift {
+  constructor(
+    private config: Config,
+    private log: Logger,
+    private document: Document,
+    private key: OctaveShiftKey,
+    private registry: NoteRenderRegistry
+  ) {}
+
+  render(): OctaveShiftRender {
+    const octaveShift = this.document.getOctaveShift(this.key);
+    const noteRenders = this.registry.get(octaveShift.id);
+    util.assertDefined(noteRenders);
+
+    const vexflowTextBrackets = this.renderVexflowTextBrackets(noteRenders);
+
+    return {
+      type: 'octaveshift',
+      key: this.key,
+      rect: Rect.empty(),
+      vexflowTextBrackets,
+    };
+  }
+
+  private renderVexflowTextBrackets(noteRenders: NoteRender[]): vexflow.TextBracket[] {
+    if (noteRenders.length < 2) {
+      this.log.warn('cannot render octave shift with less than 2 notes, skipping', {
+        octaveShiftIndex: this.key.octaveShiftIndex,
+      });
+      return [];
+    }
+
+    const vexflowTextBrackets = new Array<vexflow.TextBracket>();
+
+    const systemIndexes = util.unique(noteRenders.map((n) => n.key.systemIndex));
+    for (const systemIndex of systemIndexes) {
+      const systemNoteRenders = noteRenders.filter((n) => n.key.systemIndex === systemIndex);
+      if (systemNoteRenders.length > 1) {
+        const vexflowTextBracket = this.renderVexflowTextBracket(systemNoteRenders);
+        vexflowTextBrackets.push(vexflowTextBracket);
+      }
+    }
+
+    return vexflowTextBrackets;
+  }
+
+  private renderVexflowTextBracket(noteRenders: NoteRender[]): vexflow.TextBracket {
+    const octaveShift = this.document.getOctaveShift(this.key);
+
+    const start = noteRenders.at(0)!.vexflowTickable;
+    const stop = noteRenders.at(-1)!.vexflowTickable;
+
+    const text = Math.abs(octaveShift.size).toString();
+
+    let position: vexflow.TextBracketPosition;
+    if (octaveShift.size < 0) {
+      position = vexflow.TextBracketPosition.TOP;
+    } else {
+      position = vexflow.TextBracketPosition.BOTTOM;
+    }
+
+    let superscript: string;
+    if (octaveShift.size < 0) {
+      superscript = 'va';
+    } else {
+      superscript = 'mb';
+    }
+
+    return new vexflow.TextBracket({
+      start,
+      stop,
+      text,
+      superscript,
+      position,
+    });
+  }
+}
