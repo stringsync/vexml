e735636b58c8a2e94332c1048ba1678aee37febe
Jared Johnson <jared@jaredjohnson.dev>
2025-01-13 08:43:30 -0500

    Flesh out elements.System

--- src/elements/system.ts
+++ src/elements/system.ts
@@ -1,1 +1,28 @@
-export class System {}
+import * as rendering from '@/rendering';
+import { Logger } from '@/debug';
+
+export class System {
+  private constructor(
+    private config: rendering.Config,
+    private log: Logger,
+    private document: rendering.Document,
+    private systemRender: rendering.SystemRender
+  ) {}
+
+  static create(
+    config: rendering.Config,
+    log: Logger,
+    document: rendering.Document,
+    systemRender: rendering.SystemRender
+  ): System {
+    return new System(config, log, document, systemRender);
+  }
+
+  /** The name of the element, which can be used as a type discriminant. */
+  public readonly name = 'system';
+
+  /** Returns the bounding box of the system. */
+  get rect() {
+    return this.systemRender.rect;
+  }
+}
