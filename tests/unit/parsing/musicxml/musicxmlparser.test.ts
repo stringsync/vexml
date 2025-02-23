85063a52ad20dd01bf595422f9d6efafd55fa991
Jared Johnson <jared@jaredjohnson.dev>
2025-02-23 08:22:51 -0500

    test MusicXMLParser and fix implementation to check Document instead of XMLDocument (Document is the base class of XMLDocument)

--- tests/unit/parsing/musicxml/musicxmlparser.test.ts
+++ tests/unit/parsing/musicxml/musicxmlparser.test.ts
@@ -1,7 +1,24 @@
 import { MusicXMLParser } from '@/parsing';
+import path from 'path';
+import fs from 'fs';
+
+const MUSICXML_PATH = path.resolve(__dirname, '..', '..', '..', '__data__', 'lilypond', '01a-Pitches-Pitches.musicxml');
 
 describe(MusicXMLParser, () => {
-  it.todo('parses musicXML strings');
+  let musicXMLString: string;
 
-  it.todo('parses musicXML XMLDocuments');
+  beforeAll(() => {
+    musicXMLString = fs.readFileSync(MUSICXML_PATH).toString();
+  });
+
+  it('parses musicXML as a string', () => {
+    const parser = new MusicXMLParser();
+    expect(() => parser.parse(musicXMLString)).not.toThrow();
+  });
+
+  it('parses musicXML as a Document', () => {
+    const parser = new MusicXMLParser();
+    const document = new DOMParser().parseFromString(musicXMLString, 'application/xml');
+    expect(() => parser.parse(document)).not.toThrow();
+  });
 });
