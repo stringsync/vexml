bfa737ac8f39091edad8941ad2c04f7fba2cd654
Jared Johnson <jared@jaredjohnson.dev>
2025-02-23 12:30:28 -0500

    move addCursor param assertions and ensure partIndex is always within bounds

--- src/elements/score.ts
+++ src/elements/score.ts
@@ -65,8 +65,15 @@ export class Score {
   }
 
   addCursor(opts?: { partIndex?: number; span?: playback.CursorVerticalSpan }): playback.Cursor {
+    const partCount = this.getPartCount();
+
     const partIndex = opts?.partIndex ?? 0;
-    const span = opts?.span ?? { fromPartIndex: 0, toPartIndex: this.getPartCount() - 1 };
+    util.assert(0 <= partIndex && partIndex < partCount, 'partIndex out of bounds');
+
+    const span = opts?.span ?? { fromPartIndex: 0, toPartIndex: 0 };
+    util.assert(0 <= span.fromPartIndex && span.fromPartIndex < partCount, 'fromPartIndex out of bounds');
+    util.assert(0 <= span.toPartIndex && span.toPartIndex < partCount, 'toPartIndex out of bounds');
+    util.assert(span.fromPartIndex <= span.toPartIndex, 'fromPartIndex must be less than or equal to toPartIndex');
 
     const sequence = this.getSequences().find((sequence) => sequence.getPartIndex() === partIndex);
     util.assertDefined(sequence);
