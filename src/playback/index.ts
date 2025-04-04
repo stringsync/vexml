2e9991cb3ca07e995d50cefd8df9fd64ecf29290
Jared Johnson <jared@jaredjohnson.dev>
2025-04-04 08:49:04 -0400

    delete legacy cursor classes

--- src/playback/index.ts
+++ src/playback/index.ts
@@ -4,8 +4,5 @@ export * from './duration';
 export * from './durationrange';
 export * from './timestamplocator';
 export * from './timeline';
-export * from './legacysequence';
-export * from './legacysequencefactory';
 export * from './types';
-export * from './legacycursor';
 export * from './cursorframe';
